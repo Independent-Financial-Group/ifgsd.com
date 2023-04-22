67daeaaa6078034fb52c6bd820a2178838af658f
Alex Garcia <alex.g9415@gmail.com>
2023-04-21 23:02:24 -0700

    removed uneccessary markup

--- components/MobileNav/MobileNav.js
+++ components/MobileNav/MobileNav.js
@@ -1,15 +1,8 @@
 import React from 'react'
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon, ChevronDownIcon, ChevronRightIcon } from '@heroicons/react/24/outline'
+import { Bars3Icon, XMarkIcon, ChevronDownIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-
-const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
-]
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
