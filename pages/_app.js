37d8187ee05c3f2339f6244aa52d7970cbf8507b
Alex Garcia <alex.g9415@gmail.com>
2023-04-23 17:03:53 -0700

    added script tag for flowbite to work

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
       <Component className="box-border" {...pageProps} />
+      <Script src='../node_modules/flowbite/dist/flowbite.min.js' />
+    </>
   );
 }
 
