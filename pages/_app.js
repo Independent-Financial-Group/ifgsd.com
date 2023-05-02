bfedafb373b091284533823e5afcda432f4e12ac
Alex Garcia <agarcia@ifgsd.com>
2023-05-02 13:22:05 -0700

    added favicon to header nextjs tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
+import Head from "next/head";
 import "../styles/globals.css";
 import Script from "next/script";
+
+import favicon from "../public/logo.ico"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Head>
+      <link rel="icon" type="image/png" href={favicon} />
+    </Head>
       <Component className="box-border" {...pageProps} />
       <Script src='../node_modules/flowbite/dist/flowbite.min.js' />
     </>
