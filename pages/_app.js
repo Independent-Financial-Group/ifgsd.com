6fe3522d3461940353c72e120aeddbcfad04f5c9
Alex Garcia <>
2023-05-06 16:58:20 -0700

    update path for favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,11 @@ import Head from "next/head";
 import "../styles/globals.css";
 import Script from "next/script";
 
-import favicon from "../public/logo.ico"
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
     <Head>
-      <link rel="icon" type="image/png" href={favicon} />
+      <link rel="icon" type="image/png" href="../public/logo.ico" />
     </Head>
       <Component className="box-border" {...pageProps} />
       <Script src='../node_modules/flowbite/dist/flowbite.min.js' />
