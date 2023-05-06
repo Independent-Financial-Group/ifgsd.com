c891d9e7c25ee48cc55864792f5d935d1d69203b
Alex Garcia <agarcia@ifgsd.com>
2023-05-06 15:05:51 -0700

    changes to CTA

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,13 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import {
+  CloudArrowUpIcon,
+  LockClosedIcon,
+  ServerIcon,
+} from "@heroicons/react/20/solid";
 
 import Nav from "../components/Nav/Nav";
 import MobileNav from "../components/MobileNav/MobileNav";
@@ -13,7 +17,7 @@ import Hero from "../components/Hero/Hero";
 import Footer from "../components/Footer/Footer";
 
 import quotes from "../public/quotes.png";
-import phoneImage from "../public/phone-picking-up-compressed.jpg"
+import phoneImage from "../public/phone-picking-up-compressed.jpg";
 
 const HomePage = () => {
   return (
@@ -42,7 +46,8 @@ const HomePage = () => {
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
               transition={{ type: "spring", duration: 1, delay: 0.3 }}
-              className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+              className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0"
+            >
               <div>
                 <div className="flex flex-row items-center justify-start mb-2 gap-2">
                   <svg
@@ -270,22 +275,22 @@ const HomePage = () => {
               viewport={{ once: true }}
               transition={{ delay: 1 }}
             >
-              <h2
-                className=" absolute bg-sunburst-500 w-[265px] left-1/2 right-1/2 -translate-x-1/2 -top-8 p-2 rounded mb-8 text-4xl tracking-tight font-extrabold text-seabreeze-500 text-center drop-shadow-lg"
-              >
+              <h2 className=" absolute bg-sunburst-500 w-[265px] left-1/2 right-1/2 -translate-x-1/2 -top-8 p-2 rounded mb-8 text-4xl tracking-tight font-extrabold text-seabreeze-500 text-center drop-shadow-lg">
                 Rankings
               </h2>
             </motion.div>
-            <div
-              className="flex flex-col gap-y-5">
+            <div className="flex flex-col gap-y-5">
               <dl className="grid grid-cols-1 gap-y-16 text-center lg:grid-cols-3 py-3">
                 <motion.div
                   initial={{ opacity: 0, y: 50 }}
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
                   transition={{ delay: 0.5 }}
-                  className="mx-auto flex max-w-xs flex-col gap-y-4">
-                  <dt className="text-base leading-7 text-gray-600">Financial Advisors</dt>
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
+                  <dt className="text-base leading-7 text-gray-600">
+                    Financial Advisors
+                  </dt>
                   <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
                     600+
                   </dd>
@@ -295,7 +300,8 @@ const HomePage = () => {
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
                   transition={{ delay: 0.6 }}
-                  className="mx-auto flex max-w-xs flex-col gap-y-4">
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
                   <dt className="text-base leading-7 text-gray-600">
                     Assets Under Management
                   </dt>
@@ -308,8 +314,11 @@ const HomePage = () => {
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
                   transition={{ delay: 0.7 }}
-                  className="mx-auto flex max-w-xs flex-col gap-y-4">
-                  <dt className="text-base leading-7 text-gray-600">Highest % of Women Advisors*</dt>
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
+                  <dt className="text-base leading-7 text-gray-600">
+                    Highest % of Women Advisors*
+                  </dt>
                   <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
                     #4
                   </dd>
@@ -321,7 +330,8 @@ const HomePage = () => {
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
                   transition={{ delay: 0.8 }}
-                  className="mx-auto flex max-w-xs flex-col gap-y-4">
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
                   <dt className="text-base leading-7 text-gray-600">
                     Largest Independent Broker-Dealer*
                   </dt>
@@ -334,7 +344,8 @@ const HomePage = () => {
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
                   transition={{ delay: 0.9 }}
-                  className="mx-auto flex max-w-xs flex-col gap-y-4">
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
                   <dt className="text-base leading-7 text-gray-600">
                     Largest Privately Held Company in San Diego*
                   </dt>
@@ -347,8 +358,11 @@ const HomePage = () => {
                   whileInView={{ opacity: 1, y: 0 }}
                   viewport={{ once: true }}
                   transition={{ delay: 1 }}
-                  className="mx-auto flex max-w-xs flex-col gap-y-4">
-                  <dt className="text-base leading-7 text-gray-600">Overall Experience Satisfaction*</dt>
+                  className="mx-auto flex max-w-xs flex-col gap-y-4"
+                >
+                  <dt className="text-base leading-7 text-gray-600">
+                    Overall Experience Satisfaction*
+                  </dt>
                   <dd className="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
                     #2
                   </dd>
@@ -367,8 +381,9 @@ const HomePage = () => {
                 What our advisors say about us
               </h3>
               <p className="text-center mx-auto xl:w-1/2">
-                At IFG, we will always believe you are a name, not a number.
-                We aim to provide our representatives with investment opportunity, guidance, and planning.
+                At IFG, we will always believe you are a name, not a number. We
+                aim to provide our representatives with investment opportunity,
+                guidance, and planning.
               </p>
             </div>
             <motion.div
@@ -376,7 +391,8 @@ const HomePage = () => {
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
               transition={{ delay: 0.5, duration: 1 }}
-              className=" flex flex-col gap-y-4 lg:grid lg:grid-cols-3 gap-4 xl:absolute xl:inset-x-0 xl:-bottom-1/2 xl:max-w-screen-xl">
+              className=" flex flex-col gap-y-4 lg:grid lg:grid-cols-3 gap-4 xl:absolute xl:inset-x-0 xl:-bottom-1/2 xl:max-w-screen-xl"
+            >
               <div className="drop-shadow-lg p-8 rounded-lg bg-seabreeze-500 flex flex-col">
                 <Image
                   src={quotes}
@@ -418,7 +434,14 @@ const HomePage = () => {
                   className="mx-auto py-4"
                 />
                 <p className="text-blue-wave-900 text-sm">
-                  Since we joined the IFG family, I am consistently reminded of how cooperative and helpful IFG is of our business. Those reminders are not slogans and mission statements included at the tops of letters or the bottoms of emails. They are a real help on important matters when and where we need them. Our business supports our clients, and IFG supports our business. We are very happy to have a partner so clearly interested in helping us all grow together.
+                  Since we joined the IFG family, I am consistently reminded of
+                  how cooperative and helpful IFG is of our business. Those
+                  reminders are not slogans and mission statements included at
+                  the tops of letters or the bottoms of emails. They are a real
+                  help on important matters when and where we need them. Our
+                  business supports our clients, and IFG supports our business.
+                  We are very happy to have a partner so clearly interested in
+                  helping us all grow together.
                 </p>
                 <hr className="w-1/4 my-4 mx-auto border-sunburst-500 border-2" />
                 <div className="text-center flex flex-row justify-end items-end mt-auto">
@@ -445,7 +468,14 @@ const HomePage = () => {
                   className="mx-auto py-4"
                 />
                 <p className="text-blue-wave-900 text-sm">
-                  Hard to believe it’s been six years since we have joined the IFG family. Top rate professional and caring service team always ready to respond to needs. Conferences are top notch. Family oriented with great educational content. Consultants, management specialists, product specialists, asset management and insurance resources. A complete suite. Couldn’t ask for more in a broker dealer. Great family environment. Hats off to the management team and principals Scott, Joe and Dave.
+                  Hard to believe it’s been six years since we have joined the
+                  IFG family. Top rate professional and caring service team
+                  always ready to respond to needs. Conferences are top notch.
+                  Family oriented with great educational content. Consultants,
+                  management specialists, product specialists, asset management
+                  and insurance resources. A complete suite. Couldn’t ask for
+                  more in a broker dealer. Great family environment. Hats off to
+                  the management team and principals Scott, Joe and Dave.
                 </p>
                 <hr className="w-1/4 my-4 mx-auto border-sunburst-500 border-2" />
                 <div className="text-center flex flex-row justify-end items-end">
@@ -477,44 +507,70 @@ const HomePage = () => {
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 1, delay: 0.5 }}
                     viewport={{ once: true }}
-                    className="lg:pr-8 lg:pt-4">
+                    className="lg:pr-8 lg:pt-4"
+                  >
                     <div className="lg:max-w-lg">
-                      <h2 className="text-base font-semibold leading-7 text-sunburst-500">Join Us</h2>
-                      <p className="mt-2 text-3xl font-bold tracking-tight text-blue-wave-500 sm:text-4xl">Have ease of mind when transitioning</p>
+                      <h2 className="text-base font-semibold leading-7 text-sunburst-500">
+                        Join Us
+                      </h2>
+                      <p className="mt-2 text-3xl font-bold tracking-tight text-blue-wave-500 sm:text-4xl">
+                        Have ease of mind when transitioning
+                      </p>
                       <p className="mt-6 text-lg leading-8 text-blue-wave-900">
-                        Independence doesn't mean you're on your own. Our transition team works with your practice to ensure that you have a smooth transition.
+                        Independence doesn't mean you're on your own. Our
+                        transition team works with your practice to ensure that
+                        you have a smooth transition.
                       </p>
                       <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-blue-wave-900 lg:max-w-none">
                         <div className="relative pl-9">
                           <dt className="inline font-semibold text-blue-wave-500">
-                            <CloudArrowUpIcon className="absolute left-1 top-1 h-5 w-5 text-sunburst-500" aria-hidden="true" />
+                            <CloudArrowUpIcon
+                              className="absolute left-1 top-1 h-5 w-5 text-sunburst-500"
+                              aria-hidden="true"
+                            />
                             Phase 1
-                          </dt>{' '}
-                          <dd className="inline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eaque recusandae dolor magnam cum odit dolorem fuga possimus beatae sed?</dd>
+                          </dt>{" "}
+                          <dd className="inline">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Mollitia eaque recusandae dolor magnam cum
+                            odit dolorem fuga possimus beatae sed?
+                          </dd>
                         </div>
                       </dl>
                       <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                         <div className="relative pl-9">
                           <dt className="inline font-semibold text-blue-wave-500">
-                            <CloudArrowUpIcon className="absolute left-1 top-1 h-5 w-5 text-sunburst-500" aria-hidden="true" />
+                            <CloudArrowUpIcon
+                              className="absolute left-1 top-1 h-5 w-5 text-sunburst-500"
+                              aria-hidden="true"
+                            />
                             Phase 2
-                          </dt>{' '}
-                          <dd className="inline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eaque recusandae dolor magnam cum odit dolorem fuga possimus beatae sed?</dd>
+                          </dt>{" "}
+                          <dd className="inline">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Mollitia eaque recusandae dolor magnam cum
+                            odit dolorem fuga possimus beatae sed?
+                          </dd>
                         </div>
                       </dl>
                       <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                         <div className="relative pl-9">
                           <dt className="inline font-semibold text-blue-wave-500">
-                            <CloudArrowUpIcon className="absolute left-1 top-1 h-5 w-5 text-sunburst-500" aria-hidden="true" />
+                            <CloudArrowUpIcon
+                              className="absolute left-1 top-1 h-5 w-5 text-sunburst-500"
+                              aria-hidden="true"
+                            />
                             Phase 3
-                          </dt>{' '}
-                          <dd className="inline">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eaque recusandae dolor magnam cum odit dolorem fuga possimus beatae sed?</dd>
+                          </dt>{" "}
+                          <dd className="inline">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Mollitia eaque recusandae dolor magnam cum
+                            odit dolorem fuga possimus beatae sed?
+                          </dd>
                         </div>
                       </dl>
                     </div>
-                    <button
-                      className="rounded-md bg-blue-wave-500 px-3.5 py-2.5 text-sm font-semibold text-seabreeze-500 shadow-sm hover:bg-sunburst-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-10 mx-auto block"
-                    >
+                    <button className="rounded-md bg-blue-wave-500 px-3.5 py-2.5 text-sm font-semibold text-seabreeze-500 shadow-sm hover:bg-sunburst-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-10 mx-auto block">
                       Learn More
                     </button>
                   </motion.div>
@@ -530,22 +586,30 @@ const HomePage = () => {
             </div>
           </div>
         </section>
-        <section>
+        <section className="bg-sunburst-500">
           <div className="mx-auto max-w-screen-xl">
             <motion.div
               initial={{ opacity: 0, scale: 0 }}
               whileInView={{ opacity: 1, scale: 1 }}
               transition={{ duration: 0.5, delay: 0.5 }}
               viewport={{ once: true }}
-              className="drop-shadow-xl rounded-xl px-2 py-8" style={{
-                backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.8)), url('https://ifgsd.com/wp-content/uploads/2019/12/Optimized-IFG-Building-Cropped.jpg')",
-                backgroundSize: 'cover',
-                backgroundPosition: 'center center',
-              }}>
+              className="drop-shadow-xl rounded-xl px-2 py-20"
+              style={{}}
+            >
               <div className="mx-auto max-w-screen-sm text-center">
-                <h2 className="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-seabreeze-500 dark:text-white">Start your transition</h2>
-                <p className="mb-6 font-light text-seabreeze-100 dark:text-gray-400 md:text-lg">Get in contact with our recruiting team and schedule a home office visit to see what we're all about.</p>
-                <a href="#" className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Schedule a Visit</a>
+                <h2 className="mb-4 text-4xl tracking-tight font-extrabold leading-tight text-dune-500 dark:text-white">
+                  Start your transition
+                </h2>
+                <p className="mb-6 text-dune-500 dark:text-gray-400 md:text-lg">
+                  Get in contact with our recruiting team and schedule a home
+                  office visit to see what we're all about.
+                </p>
+                <a
+                  href="#"
+                  className="text-sunburst-500 bg-dune-500 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-semibold rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                >
+                  Schedule a Visit
+                </a>
               </div>
             </motion.div>
           </div>
