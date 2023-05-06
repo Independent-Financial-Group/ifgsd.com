5b8b013a5f544b2876c3a49a6d26df87f46ebe2a
Alex Garcia <>
2023-05-06 15:32:05 -0700

    comment out categories for now

--- pages/press-media/press-releases/index.js
+++ pages/press-media/press-releases/index.js
@@ -105,13 +105,13 @@ const index = ({formattedPressReleases}) => {
               ))}
             </div>
             <aside className="mx-auto mt-10 xl:grid max-w-2xl xl:grid-cols-1 gap-x-8 gap-y-16 border-t border-sunburst-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none xl:col-span-2 hidden">
-                Categories
+                {/* Categories
                 <ul>
                   <li>press releases</li>
                   <li>blog</li>
                   <li>podcast</li>
                   <li></li>
-                </ul>
+                </ul> */}
             </aside>
           </div>
         </div>
