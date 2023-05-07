7b3b4c9c3be49d937c9901a1f18011f47977def7
Alex Garcia <>
2023-05-06 17:01:46 -0700

    fixed duplicate imports

--- pages/press-media/blog/index.js
+++ pages/press-media/blog/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import Nav from "../../../components/Nav/Nav";
 import MobileNav from "../../../components/MobileNav/MobileNav";
 import Footer from "../../../components/Footer/Footer";
-import Link from "next/link";
 
 const contenful = require("contentful");
 
