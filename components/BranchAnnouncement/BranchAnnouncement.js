bc8ceb98112a7c23d0405f5f9ee3328355d7cde7
Alex Garcia <>
2023-08-09 12:12:32 -0700

    z-index issues

--- components/BranchAnnouncement/BranchAnnouncement.js
+++ components/BranchAnnouncement/BranchAnnouncement.js
@@ -22,17 +22,34 @@ const BranchAnnouncement = ({ newBranches }) => {
                 whileInView={{ opacity: 1, y: 0 }}
                 viewport={{ once: true }}
                 transition={{ type: "spring", duration: 6, delay: 0.3 }}
-                className="mx-auto max-w-screen-2xl text-center px-4 py-16 sm:px-6 sm:py-12 absolute inset-10"
+                className="mx-auto max-w-screen-2xl text-center px-4 py-16 sm:px-6 sm:py-12 absolute inset-0"
             >
-                <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-seabreeze-500 bt-2'>New Rep Announcement</h2>
+                <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-seabreeze-500 border-y-4 py-4 w-1/3 mx-auto'>New Rep Announcement</h2>
                 <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-seabreeze-500'>{newBranches[0].dba} joins IFG</h2>
                 <p className='mb-8 w-1/2 m-auto'>IFG welcomes {newBranches[0].reps} with {newBranches[0].dba} from {newBranches[0].officeLocation}!</p>
-                <Link className='p-4 rounded bg-sunburst-500' href="#">Read their story</Link>
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0}}
+                whileInView={{ opacity: 1}}
+                viewport={{ once: true }}
+                transition={{ type: "spring", duration: 1, delay: 2 }}
+                className="mx-auto max-w-screen-2xl text-center px-4 py-16 sm:px-6 sm:py-12 absolute inset-60 z-10"
+            >
+                <Link className='p-4 rounded bg-sunburst-500' href="/">Read their story</Link>
             </motion.div>
             <Image style={{ width: '100%' }} className='absolute bottom-0' src={newRepBanner3} />
             <motion.div
-                initial={{x: 100 }}
-                whileInView={{x: -100 }}
+                initial={{ x: 100 }}
+                whileInView={{ x: -40 }}
+                // viewport={{ once: true }}
+                transition={{ type: "spring", duration: 3 }}
+                className=' absolute right-0 bottom-0'
+            >
+                <Image src={newRepBanner4} />
+            </motion.div>
+            <motion.div
+                initial={{ x: 100 }}
+                whileInView={{ x: -20 }}
                 // viewport={{ once: true }}
                 transition={{ type: "spring", duration: 6 }}
                 className=' absolute right-0 bottom-0'
@@ -40,19 +57,10 @@ const BranchAnnouncement = ({ newBranches }) => {
                 <Image src={newRepBanner4} />
             </motion.div>
             <motion.div
-                initial={{x: 100 }}
-                whileInView={{x: -50 }}
-                // viewport={{ once: true }}
-                transition={{ type: "spring", duration: 6, delay: 0.9 }}
-                className=' absolute right-0 bottom-0'
-            >
-                <Image src={newRepBanner4} />
-            </motion.div>
-            <motion.div
-                initial={{x: 100 }}
-                whileInView={{x: 0 }}
-                // viewport={{ once: true }}
-                transition={{ type: "spring", duration: 6, delay: 1 }}
+                initial={{ x: 100 }}
+                whileInView={{ x: 0 }}
+                viewport={{ once: true }}
+                transition={{ type: "spring", duration: 6 }}
                 className=' absolute right-0 bottom-5'
             >
                 <Image src={newRepBanner4} />
