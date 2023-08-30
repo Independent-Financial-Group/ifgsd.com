8ec64119be76c910f6ec106e17c456d96da20966
Alex Garcia <>
2023-08-30 14:21:13 -0700

    new component and page

--- pages/careers/work-at-ifg/index.js
+++ pages/careers/work-at-ifg/index.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import Script from 'next/script'
-import Head from 'next/head'
+import Link from 'next/link'
 import Nav from '../../../components/Nav/Nav'
 import Footer from '../../../components/Footer/Footer'
+import JobBoard from '../../../components/JobBoard/JobBoard'
 
-const index = () => {
+export const getServerSideProps = async () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      authorization: `Basic ${process.env.NEXT_PUBLIC_BAMBOOHR_KEY_MERCEDES}`
+    }
+  };
+
+  const res = await fetch('https://api.bamboohr.com/api/gateway.php/ifgsd/v1/applicant_tracking/jobs?statusGroups=Open&sortBy=department', options)
+  const data = await res.json()
+
+  return { props: { data } }
+}
+
+const index = ({data}) => {
 
   return (
     <>
       <Nav />
       <main>
         <section className='max-w-screen-xl mx-auto my-32'>
+          <div>
+          <JobBoard jobs={data}/>
+          </div>
         </section>
       </main>
       <Footer />
