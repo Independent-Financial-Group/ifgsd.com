38cd6682c65d44a01c11ae960cf09977ab70f5bc
Alex Garcia <>
2023-08-31 16:09:15 -0700

    sidebar for portal dashboard

--- pages/app/index.js
+++ pages/app/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import Sidebar from '../../components/App/Sidebar/Sidebar'
+import { useUser } from '@clerk/nextjs'
 
 const index = () => {
+  const { isLoaded, isSignedIn, user } = useUser()
+
+  if(!isLoaded || !isSignedIn) {
+    return null;
+  }
+
   return (
-    <div>Welcome to Independent Financial Group's Rep Portal!</div>
+    <div className='m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500'>
+      <Sidebar userInfo={user}/>
+    </div>
   )
 }
 
