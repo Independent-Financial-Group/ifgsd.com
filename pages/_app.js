1fa4b85d4099bed3c1fde5ff78716b2e4c87972e
Alex Garcia <>
2023-08-31 12:09:18 -0700

    added clerkjs authentication and public routes

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,18 @@ import Head from "next/head";
 import "../styles/globals.css";
 import Script from "next/script";
 import NextNProgress from 'nextjs-progressbar';
+import { ClerkProvider } from '@clerk/nextjs'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <link rel="icon" type="image/png" href="/logo.ico" />
-    </Head>
+      <Head>
+        <link rel="icon" type="image/png" href="/logo.ico" />
+      </Head>
       <NextNProgress />
-      <Component className="box-border" {...pageProps} />
+      <ClerkProvider {...pageProps}>
+        <Component className="box-border" {...pageProps} />
+      </ClerkProvider>
       <Script src='../node_modules/flowbite/dist/flowbite.min.js' />
     </>
   );
