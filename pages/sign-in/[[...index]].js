5575a0d2e4e6b5d2bbb8298233bdf96fe4d2cca6
Alex Garcia <>
2023-09-05 13:49:21 -0700

    login screen visuals

--- pages/sign-in/[[...index]].js
+++ pages/sign-in/[[...index]].js
@@ -4,8 +4,16 @@ import { SignIn } from '@clerk/nextjs'
 const index = () => {
   return (
     <>
-      <div className='relative'>
-        <SignIn className="absolute right-0" />
+      <div className='h-[100vh] bg-[url(/sign-in/bg-coronado-compressed.jpg)] bg-no-repeat bg-cover relative'>
+        <SignIn
+          appearance={{
+            elements: {
+              rootBox:"absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2",
+              logoBox: "justify-center",
+              logoImage: "!max-w-[200px] !h-auto"
+            }
+          }}
+        />
       </div>
     </>
   )
