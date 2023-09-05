3376caa8d2ad3050a522fb8b0bf2c34f78465454
Alex Garcia <>
2023-09-05 13:59:14 -0700

    style

--- pages/app/account/[[...index]].js
+++ pages/app/account/[[...index]].js
@@ -3,8 +3,16 @@ import { UserProfile } from '@clerk/nextjs'
 
 const profile = () => {
   return (
-    <div className='max-w-screen-xl mx-auto relative h-[100vh] outline'>
-        <UserProfile path='/app/account' routing='path' />
+    <div className='bg-[url(/sign-in/bg-coronado-compressed.jpg)] bg-no-repeat bg-cover'>
+      <div className='max-w-screen-xl mx-auto relative h-[100vh] relative '>
+        <UserProfile
+          appearance={{
+            elements: {
+              rootBox: "absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2"
+            }
+          }}
+          path='/app/account' routing='path' />
+      </div>
     </div>
   )
 }
