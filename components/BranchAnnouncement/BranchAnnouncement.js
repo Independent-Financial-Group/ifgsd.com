2f4a7078c7feed1068f2250dd54839fb824e6ea8
Alex Garcia <>
2023-09-11 15:18:43 -0700

    update variable names

--- components/BranchAnnouncement/BranchAnnouncement.js
+++ components/BranchAnnouncement/BranchAnnouncement.js
@@ -4,9 +4,9 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
 
-const BranchAnnouncement = ({ newBranches }) => {
+const BranchAnnouncement = ({ newBranch }) => {
 
-    // useEffect(() => { console.log(`Branches: ${JSON.stringify(newBranches, null, 3)}`) }, [])
+    const fields = newBranch.fields;
 
     return (
         <div className='bg-blue-wave-700/10 text-seabreeze-500 h-[500px] relative overflow-hidden rounded-[40px]'>
@@ -16,7 +16,7 @@ const BranchAnnouncement = ({ newBranches }) => {
                 </div>
                 <div className='text-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
                     <h2 className='text-4xl lg:text-7xl font-bold uppercase'>IFG Welcomes</h2>
-                    <p className='font-semibold text-lg lg:text-xl my-4'>{newBranches[0].reps} with {newBranches[0].dba} from {newBranches[0].officeLocation}</p>
+                    <p className='font-semibold text-lg lg:text-xl my-4'>{`${fields.repNames[0]} ${fields.repNames[1]}`} with {fields.dba} from {fields.officeLocation}</p>
                     <Link className='font-bold bg-neon-orange-500 py-2 px-4 rounded-lg' href="#">Read More</Link>
                 </div>
             </div>
