52b2f7c29acc38fea15504790872594fa17553ae
Alex Garcia <>
2023-09-15 15:06:42 -0700

    Fix image scaling and headings

--- pages/join/why-choose-us/our-mission.js
+++ pages/join/why-choose-us/our-mission.js
@@ -9,7 +9,7 @@ const ourMission = () => {
             <section className='bg-hazard-blue-500 h-[20vh] relative mb-32'>
                 <Container>
                     <div className='absolute top-1/2 -translate-y-1/2'>
-                        <h2 className='text-2xl font-bold text-white'><img src="/graphicAssets/triangle-highlight.png" className='inline mr-2' />Committed to Excellence</h2>
+                        <h2 className='text-2xl font-bold text-white'>Committed to Excellence</h2>
                         <h1 className='lg:text-6xl font-bold text-white'>Our Mission</h1>
                     </div>
                 </Container>
@@ -18,8 +18,8 @@ const ourMission = () => {
                 <Container>
                     <div className='lg:flex lg:gap-5'>
                         <div>
-                            <h2 className='text-3xl font-bold text-hazard-blue-500'>Personalized Services</h2>
-                            <p className='text-lg'>Independent Financial Group is a comprehensive Independent Financial Services company committed to the support, success and independence of our financial advisors and their clients. We provide personalized service, common sense compliance and access to high quality products, technology and practice management solutions. We actively encourage an environment that balances business and family; where employees are valued, provided quality benefits and a supportive work environment that encourages personal and professional growth.</p>
+                            <h2 className='text-3xl font-bold'><img src="/graphicAssets/triangle-highlight.png" alt="ornament" className='inline mr-4' /> Personalized Services</h2>
+                            <p className='text-lg my-6'>Independent Financial Group is a comprehensive Independent Financial Services company committed to the support, success and independence of our financial advisors and their clients. We provide personalized service, common sense compliance and access to high quality products, technology and practice management solutions. We actively encourage an environment that balances business and family; where employees are valued, provided quality benefits and a supportive work environment that encourages personal and professional growth.</p>
                         </div>
                         <div className='relative'>
                             <img className='absolute -right-9 top-1/2 -translate-y-1/2' src="/graphicAssets/CTA-ornament.png" />
@@ -54,14 +54,14 @@ const ourMission = () => {
                 <Container>
                     <div className='lg:grid lg:grid-cols-2 lg:gap-5'>
                         <div className='self-center'>
-                            <h2 className='text-3xl font-bold'><img src="/graphicAssets/triangle-highlight.png" alt="ornament" className='inline mr-4' />Experience independence.</h2>
+                            <h2 className='text-3xl font-bold'><img src="/graphicAssets/triangle-highlight.png" alt="ornament" className='inline mr-4' />Experience Independence.</h2>
                             <p className='my-6 text-lg'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore alias nemo nulla nam. Vel recusandae saepe a veritatis quo. Laudantium aspernatur recusandae, facilis blanditiis accusantium impedit reiciendis eos cum hic corporis voluptatem dolore est, vitae repellendus ipsa nostrum itaque perferendis dignissimos. A ab iure vel odit, voluptatum eaque repellendus perspiciatis.</p>
-                            <Link href="/join/contact" className='border-2 border-neon-orange-500 px-4 py-2 text-neon-orange-500 font-bold rounded'>Talk to an IFG Rep</Link>
+                            <Link href="/join/contact" className='border-2 border-hazard-blue-500 px-4 py-2 text-hazard-blue-500 font-bold rounded'>Talk to an IFG Rep</Link>
                         </div>
-                        <div className='lg:w-1/2 place-self-center'>
+                        <div className='place-self-end'>
                             <div className='relative'>
                                 <img src='/graphicAssets/CTA-ornament.png' alt='ornament' className='absolute left-1/2 -translate-x-1/2 -bottom-10'/>
-                                <img className='rounded-lg' src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3149&q=80" alt="Financial Professional" />
+                                <img className=' max-auto rounded-lg h-[500px] w-[450px] object-top object-cover' src="https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3149&q=80" alt="Financial Professional" />
                             </div>
                         </div>
                     </div>
