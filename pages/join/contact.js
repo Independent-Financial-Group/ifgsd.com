cc9c65ecfc9b48b6c3eae3535912b052fcf9c288
Alex Garcia <>
2023-09-18 10:57:24 -0700

    new contact page

---
+++ pages/join/contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PublicLayout from "../../components/PublicLayout/PublicLayout";
+import Container from "../../components/Container/Container";
+
+const contact = () => {
+  return (
+    <PublicLayout>
+      <Container>
+        <h1>Contact Page</h1>
+      </Container>
+    </PublicLayout>
+  );
+};
+
+export default contact;
