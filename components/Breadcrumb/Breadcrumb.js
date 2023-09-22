9ef7746b18b2c8f081825320e57f1c47c410cde0
Alex Garcia <>
2023-09-22 12:53:12 -0700

    new component

---
+++ components/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const Breadcrumb = () => {
+  const router = useRouter();
+  const convertBreadcrumb = (string) => {
+    return string
+      .replace(/-/g, " ")
+      .replace(/oe/g, "ö")
+      .replace(/ae/g, "ä")
+      .replace(/ue/g, "ü")
+      .toUpperCase();
+  };
+  const [breadcrumbs, setBreadcrumbs] = useState(null);
+
+  useEffect(() => {
+    if (router) {
+      const linkPath = router.asPath.split("/");
+      linkPath.shift();
+
+      const pathArray = linkPath.map((path, i) => {
+        return {
+          breadcrumb: path,
+          href: "/" + linkPath.slice(0, i + 1).join("/"),
+        };
+      });
+
+      setBreadcrumbs(pathArray);
+    }
+  }, [router]);
+
+  if (!breadcrumbs) {
+    return null;
+  }
+
+  console.log(breadcrumbs)
+
+  return (
+    <>
+      {/*
+  Heads up! 👋
+
+  This component comes with some `rtl` classes. Please remove them if they are not needed in your project.
+*/}
+
+      <nav aria-label="Breadcrumb" className="my-8">
+        <ol className="flex items-center gap-1 text-sm text-gray-600">
+          <li>
+            <Link href="/" className="block transition hover:text-gray-700">
+              <span className="sr-only"> Home </span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+            </Link>
+          </li>
+
+          {breadcrumbs.map((breadcrumb, i) => {
+            return (
+              <>
+                <li className="rtl:rotate-180">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </li>
+
+                <li>
+                  <Link href={breadcrumb.href} className="block transition hover:text-gray-700">
+                    {convertBreadcrumb(breadcrumb.breadcrumb)}
+                  </Link>
+                </li>
+              </>
+            );
+          })}
+        </ol>
+      </nav>
+    </>
+  );
+};
+
+export default Breadcrumb;
