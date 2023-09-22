f24f868ff0ef3c7ae385ef18aa8e83fd992e9b38
Alex Garcia <>
2023-09-22 13:45:00 -0700

    update for responsive design

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children, classes }) => {
   return (
-    <div className={`px-4 lg:mx-auto lg:max-w-screen-xl lg:px-0 ${classes}`}>
+    <div className={`px-5 mx-auto max-w-screen-xl${classes}`}>
       {children}
     </div>
   );
