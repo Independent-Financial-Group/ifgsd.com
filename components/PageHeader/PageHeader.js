ca839a368fa1b90b3c9f64fcc93ecb3802b4c69c
Alex Garcia <>
2023-10-31 09:18:00 -0700

    fix corner rounding for mobile

--- components/PageHeader/PageHeader.js
+++ components/PageHeader/PageHeader.js
@@ -4,8 +4,8 @@ import Container from "../Container/Container";
 
 const PageHeader = ({children, bgPath}) => {
   return (
-    <header className={`relative h-[calc(75vh-60px)] rounded-bl-[40px] rounded-br-[40px] bg-cover bg-center bg-no-repeat md:h-[calc(35vh-60px)] lg:h-[calc(50vh-60px)] xl:h-[calc(50vh-60px)] ${bgPath}`}>
-      <div className="h-full rounded-bl-[40px] rounded-br-[40px] bg-dunkel-blue-900/20">
+    <header className={`relative h-[calc(75vh-60px)] rounded-bl-[20px] rounded-br-[20px] lg:rounded-bl-[40px] lg:rounded-br-[40px] bg-cover bg-center bg-no-repeat md:h-[calc(35vh-60px)] lg:h-[calc(50vh-60px)] xl:h-[calc(50vh-60px)] ${bgPath}`}>
+      <div className="h-full rounded-bl-[20px] rounded-br-[20px] lg:rounded-bl-[40px] lg:rounded-br-[40px] bg-dunkel-blue-900/20">
         <Container>
         <div className="absolute top-1/2 -translate-y-1/2">
           {children}
