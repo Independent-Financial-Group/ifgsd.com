42baf555868fb0faa7e0bceb870578f63a1ad270
Alex Garcia <>
2023-11-01 15:29:30 -0700

    add h-full and fix responsive design

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children, classes }) => {
   return (
-    <div className={`px-5 mx-auto max-w-screen-xl ${classes}`}>
+    <div className={`px-5 mx-auto max-w-screen-xl h-full ${classes}`}>
       {children}
     </div>
   );
--- components/PageHeader/PageHeader.js
+++ components/PageHeader/PageHeader.js
@@ -7,10 +7,10 @@ const PageHeader = ({ children, bgPath, centerElement }) => {
     <header
       className={`relative h-[calc(75vh-60px)] rounded-bl-[20px] rounded-br-[20px] bg-cover bg-center bg-no-repeat md:h-[calc(35vh-60px)] lg:h-[calc(50vh-60px)] lg:rounded-bl-[40px] lg:rounded-br-[40px] xl:h-[calc(50vh-60px)] ${bgPath}`}
     >
-      <div className="h-full rounded-bl-[20px] rounded-br-[20px] bg-dunkel-blue-900/20 lg:rounded-bl-[40px] lg:rounded-br-[40px]">
+      <div className="h-full rounded-bl-[20px] rounded-br-[20px] bg-dunkel-blue-900/25 lg:rounded-bl-[40px] lg:rounded-br-[40px]">
         <Container>
           {centerElement && (
-            <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            <div className="flex h-full w-full items-center lg:w-auto lg:h-auto lg:absolute lg:left-1/2  lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2">
               {children}
             </div>
           )}
