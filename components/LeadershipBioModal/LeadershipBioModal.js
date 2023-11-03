01b7c6ca1978171678dfe4322c15d02308e86fc6
Alex Garcia <>
2023-11-03 10:27:24 -0700

    remove console logs

--- components/LeadershipBioModal/LeadershipBioModal.js
+++ components/LeadershipBioModal/LeadershipBioModal.js
@@ -28,11 +28,7 @@ const LeadeshipBioModal = ({ openModal, setOpenModal, entryId }) => {
     if (openModal !== "default") {
       setBio(null);
     }
-
-    console.log(openModal);
   }, [entryId, openModal]);
-
-  console.log(bio);
 
   if (bio === null) {
     return null;
@@ -71,10 +67,24 @@ const LeadeshipBioModal = ({ openModal, setOpenModal, entryId }) => {
             <Spinner />
           ) : (
             <a
-              className="text-right"
+              className="flex w-full justify-end gap-2 font-bold text-neon-orange-500"
               href={bio.linkedInProfile}
               target="_blank"
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="h-6 w-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                />
+              </svg>
               LinkedIn
             </a>
           )}
