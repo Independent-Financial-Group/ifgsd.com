df2f195a16b7e136e5c44ba28831238a8a881f5d
Alex Garcia <>
2023-11-03 10:27:03 -0700

    finalize page

--- pages/about/leadership.js
+++ pages/about/leadership.js
@@ -1,8 +1,23 @@
 import React, { useState } from "react";
+
+// NEXT IMPORTS
+import Image from "next/image";
+
+// IMPORT COMPONENTS
 import PublicLayout from "../../components/PublicLayout/PublicLayout";
 import LeadershipBioModal from "../../components/LeadershipBioModal/LeadershipBioModal";
 import Head from "next/head";
 import Container from "../../components/Container/Container";
+import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
+import PageHeader from "../../components/PageHeader/PageHeader";
+
+// IMPORT ASSETS
+import dave from "../../public/_leadership/images/dfischer-headshot.jpg";
+import scott from "../../public/_leadership/images/sheising-headshot.jpg";
+import orangeCircle from "../../public/_leadership/graphics/circle-orange.png";
+import GradientLeft from "../../public/_leadership/graphics/blur-gradient-left.png";
+import gradientRight from "../../public/_leadership/graphics/blur-gradient-right.png";
+import oranmentBlue from "../../public/_leadership/graphics/ornament-blue.png";
 
 // CONTENTFUL IMPORTS
 const contenful = require("contentful");
@@ -13,8 +28,11 @@ const client = contenful.createClient({
 });
 
 export async function getStaticProps() {
-
-  const initialData = await client.getEntries({ content_type: "leadership", order: "sys.createdAt" });
+  const initialData = await client.getEntries({
+    content_type: "leadership",
+    "metadata.tags.sys.id[in]": "seniorLeadership",
+    order: "sys.createdAt",
+  });
 
   const formattedData = initialData.items.map((item) => {
     return {
@@ -42,47 +60,66 @@ const leadership = ({ formattedData: data, initialData }) => {
         <title>Leadership Team | Independent Financial Group San Diego</title>
       </Head>
       <PublicLayout>
-        <section className="relative mb-32 h-[50vh] rounded-b-[40px] bg-[url('/backgrounds/wave-background-abstract-compressed.webp')] bg-cover bg-center">
-          <div className="h-full w-full rounded-b-[40px] bg-hazard-blue-500/60">
-            <Container>
-              <div className="absolute left-1/2 top-1/2 max-w-prose -translate-x-1/2 -translate-y-1/2 text-center text-seabreeze-500">
-                <h1 className="mb-5 text-7xl font-bold ">
-                  Our Leadership Team
-                </h1>
-                <p className="text-lg">
-                  Independent Financial Group’s leadership team leverages years
-                  of successful financial expertise to craft a top-tier
-                  broker-dealer framework.
-                </p>
-              </div>
-            </Container>
+        <PageHeader
+          bgPath={"bg-[url('/_leadership/images/hero.webp')]"}
+          centerElement
+        >
+          <div>
+            <h1 className="mb-8 text-center text-5xl font-bold text-seabreeze-500">
+              Our Leadership Team
+            </h1>
+            <p className="text-center text-xl text-seabreeze-500 lg:mx-auto lg:w-3/4">
+              Independent Financial Group&apos;s leadership team leverages years
+              of financial expertise to craft a top-tier broker-dealer.
+            </p>
           </div>
-        </section>
+        </PageHeader>
         <section className="mb-32">
           <Container>
-            <h2 className="mb-5 text-center text-3xl font-bold text-neon-orange-500">
+            <Image
+              src={orangeCircle}
+              alt="decorative circle graphic"
+              className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 md:top-1/3"
+            />
+            <Breadcrumb />
+            <h2 className="mb-5 text-center text-4xl font-bold text-neon-orange-500">
               Founders
             </h2>
-            <div className="flex flex-row justify-center gap-5">
+            <div className="relative grid grid-cols-2 justify-center gap-5">
+              <Image
+                src={oranmentBlue}
+                className="absolute left-1/2 top-1/2 hidden -translate-x-1/2 -translate-y-1/2 lg:block"
+                alt="decorative ornament graphic"
+              />
               <div>
-                <img
+                <Image
                   className="mx-auto mb-5 rounded-[20px]"
-                  src="https://placehold.co/500"
+                  src={scott}
+                  alt="Portrait of IFG founder Scott Heising"
                 />
                 <h3 className="text-xl font-bold">Scott Heising</h3>
                 <p className="text-bold">President, CEO, & Founder</p>
-                <button className="font-bold text-neon-orange-500">
+                <button
+                  onClick={handleClick}
+                  data-id="1SqrtnbS3q0nD35NMb8Lk0"
+                  className="font-bold text-neon-orange-500"
+                >
                   Read Bio &rarr;
                 </button>
               </div>
               <div>
-                <img
+                <Image
                   className="mx-auto mb-5 rounded-[20px]"
-                  src="https://placehold.co/500"
+                  src={dave}
+                  alt="portrait of IFG co-founder Dave Fischer"
                 />
                 <h3 className="text-xl font-bold">David Fischer</h3>
                 <p className="text-bold">CMO</p>
-                <button className="font-bold text-neon-orange-500">
+                <button
+                  data-id="60UCpXYh3gLMMrUu7WaqZg"
+                  onClick={handleClick}
+                  className="font-bold text-neon-orange-500"
+                >
                   Read Bio &rarr;
                 </button>
               </div>
@@ -91,13 +128,34 @@ const leadership = ({ formattedData: data, initialData }) => {
         </section>
         <section>
           <Container>
-            <h2 className="mb-5 text-center text-3xl font-bold text-neon-orange-500">
+            <h2 className="mb-10 text-center text-4xl font-bold text-hazard-blue-500">
               Senior Leadership
             </h2>
-            <div className="grid grid-cols-3 gap-5">
-              {data.map((person) => {
+            <div className="grid grid-cols-4 gap-x-5 gap-y-10 md:grid-cols-6 lg:gap-x-0">
+              <Image
+                src={gradientRight}
+                alt="decorative graphic"
+                className="absolute -right-1/4 -z-10 lg:right-0"
+              />
+              <Image
+                src={GradientLeft}
+                alt="decorative graphic"
+                className="absolute -left-1/4 top-full -z-10 lg:left-0"
+              />
+              {data.map((person, i) => {
                 return (
-                  <div className="leadership-card" key={person.id}>
+                  <div
+                    className={
+                      i >= 6
+                        ? `${
+                            i >= 6
+                              ? "col-span-2 md:col-span-3 md:justify-self-center"
+                              : ""
+                          }`
+                        : "col-span-2"
+                    }
+                    key={person.id}
+                  >
                     <img
                       className="mx-auto mb-5 w-[254px] rounded-bl-full rounded-br-full rounded-tl-full"
                       src={`https:${person.headshot.fields.file.url}`}
