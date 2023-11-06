4530349839534dff3ba983657459b2e4cd2dcc8b
Alex Garcia <>
2023-11-06 15:16:18 -0800

    finish page

--- pages/about/our-story.js
+++ pages/about/our-story.js
@@ -11,13 +11,19 @@ import Container from "../../components/Container/Container";
 import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import Timeline from "../../components/Timeline/Timeline";
-
 import ReactBeforeSliderComponent from "react-before-after-slider-component";
 import "react-before-after-slider-component/dist/build.css";
+import Button from "../../components/Button/Button";
 
 // IMPORT ASSETS
 import highlight from "../../public/_global-graphics/highlight.png";
 import highlightBlue from "../../public/_global-graphics/highlight-blue.png";
+import gradientWave from "../../public/_success-story/graphics/gradient-wave.png";
+import ctaImage from "../../public/_success-story/images/cta.png";
+import imageHighlightOrange from "../../public/_global-graphics/image-highlight-orange.png";
+import imageHighlightBlue from "../../public/_global-graphics/image-highlight-blue.png";
+
+// CONTENTFUL
 
 const contenful = require("contentful");
 
@@ -46,8 +52,6 @@ const ourStory = ({ timeline }) => {
   const then = { imageUrl: "/_success-story/images/then.jpg" };
   const now = { imageUrl: "/_success-story/images/now.jpg" };
 
-  console.log(timeline);
-
   return (
     <>
       <Head>
@@ -69,12 +73,12 @@ const ourStory = ({ timeline }) => {
             </h1>
           </div>
         </PageHeader>
-        <section className="my-32">
+        <section className="my-10 lg:my-32">
           <Container>
             <Breadcrumb />
             <div className="gap-5 lg:grid lg:grid-cols-2">
-              <div className="self-center">
-                <h2 className="flex items-center gap-2 text-3xl font-bold text-hazard-blue-500 ">
+              <div className="mb-10 self-center lg:mb-0">
+                <h2 className="mb-8 flex items-center gap-2 text-3xl font-bold text-hazard-blue-500">
                   <Image src={highlight} alt="decorative heading highlight" />
                   Built on a handshake
                 </h2>
@@ -102,7 +106,7 @@ const ourStory = ({ timeline }) => {
             </div>
           </Container>
         </section>
-        <section className="my-32 bg-[url('/_success-story/images/time.jpg')] bg-cover bg-center">
+        <section className="my-10 bg-[url('/_success-story/images/time.jpg')] bg-cover bg-center lg:my-32">
           <div className="h-full bg-dunkel-blue-900/25 py-10">
             <Container>
               <h2 className="mb-10 text-center text-3xl font-bold text-seabreeze-500">
@@ -111,6 +115,41 @@ const ourStory = ({ timeline }) => {
               <Timeline data={timeline} />
             </Container>
           </div>
+        </section>
+        <section className="relative my-10 lg:my-32">
+          <Image
+            src={gradientWave}
+            className="absolute bottom-0 -z-10 w-full lg:-bottom-16"
+          />
+          <Container>
+            <div className="lg:grid lg:grid-cols-2">
+              <div className="mb-10 lg:mb-0">
+                <h2 className="mb-10 flex items-center gap-2 text-3xl font-bold text-hazard-blue-500">
+                  <Image src={highlight} alt="decorative heading highlight" />{" "}
+                  Let us be a part of your success story
+                </h2>
+                <p className="mb-10 text-xl">
+                  We&apos;re here to take your business to the next level.
+                </p>
+                <Button href={"/join/contact"} type={"primary"}>
+                  Contact Us
+                </Button>
+              </div>
+              <div className="relative">
+                <Image
+                  src={imageHighlightBlue}
+                  alt="decorative image ornament"
+                  className="absolute -top-3"
+                />
+                <Image
+                  src={imageHighlightOrange}
+                  alt="decorative image ornament"
+                  className="absolute -bottom-3 right-0"
+                />
+                <Image src={ctaImage} />
+              </div>
+            </div>
+          </Container>
         </section>
       </PublicLayout>
     </>
