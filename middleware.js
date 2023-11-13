9050b9ac25fc5a29c6f8d9147901db319b90f086
Alex Garcia <>
2023-11-13 10:48:09 -0800

    open route to reg bi page

--- middleware.js
+++ middleware.js
@@ -6,6 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
+    "/reg-bi-investor",
     "/join/(.*)",
     "/about/(.*)",
     "/careers(.*)",
