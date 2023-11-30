af8ea296dc456a9a3bcf6937da98bd26af2b9996
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2023-11-30 11:23:01 -0800

    add date to slug page

--- pages/press-and-media/press-releases/[slug].js
+++ pages/press-and-media/press-releases/[slug].js
@@ -55,7 +55,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: 'blocking'
+    fallback: true,
   };
 };
 
@@ -110,42 +110,54 @@ const pressRelease = ({ release }) => {
       <PublicLayout>
         <header className="relative h-[calc(75vh-60px)] bg-neon-orange-500 bg-cover bg-center bg-no-repeat md:h-[calc(70vh-60px)] lg:h-[calc(50vh-60px)] xl:h-[calc(50vh-60px)]">
           <Container>
-            <h1 className="absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/3 text-center text-4xl text-seabreeze-500">
-              {release.title}
-            </h1>
-            <Image
-              src={`https:${release.image.fields.file.url}`}
-              alt={release.image.fields.title}
-              width={release.image.fields.file.details.image.width}
-              height={release.image.fields.file.details.image.height}
-              className="absolute -bottom-1/3 left-1/2 -translate-x-1/2 rounded-[20px] object-cover lg:-bottom-3/4 lg:h-[650px] lg:w-[1280px]"
-            />
+            <div className="absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/3 ">
+              <p className="text-center text-base text-seabreeze-500">
+                - Press Release -
+              </p>
+              <h1 className="my-5 text-center text-4xl text-seabreeze-500">
+                {release.title}
+              </h1>
+              <p className="text-center text-base text-seabreeze-500">
+                {release.date}
+              </p>
+            </div>
           </Container>
         </header>
-        <section className="relative mt-[300px] lg:mt-[500px]">
+        <section className="relative">
           <Container>
-            <div className="flex flex-col items-center">
+            <div className="absolute -top-14 left-1/2 -translate-x-1/2">
               <Image
-                src={`https:${release.editor.photo.fields.file.url}`}
-                height={release.editor.photo.fields.file.details.image.height}
-                width={release.editor.photo.fields.file.details.image.width}
-                alt={`Image of the editor of this article, ${release.editor.fullName}`}
-                className="h-[100px] w-[100px] rounded-full"
+                src={`https:${release.image.fields.file.url}`}
+                alt={release.image.fields.title}
+                width={release.image.fields.file.details.image.width}
+                height={release.image.fields.file.details.image.height}
+                className="rounded-[20px] object-fill"
               />
-              <h2 className=" text-base font-bold text-neon-orange-500">
-                {release.editor.fullName}
-              </h2>
-              <p className="text-center text-sm">{release.editor.role}</p>
-              <a
-                href="mailto:psaunders@ifgsd.com"
-                className="text-xs font-bold underline"
-              >
-                Press Contact
-              </a>
             </div>
-            <article className="mx-auto my-10 max-w-prose rounded-lg bg-white px-8 py-10">
-              {documentToReactComponents(release.writtenContent, options)}
-            </article>
+            <div className="pt-36 md:pt-72 lg:pt-[34rem]">
+              <div className="flex flex-col items-center">
+                <Image
+                  src={`https:${release.editor.photo.fields.file.url}`}
+                  height={release.editor.photo.fields.file.details.image.height}
+                  width={release.editor.photo.fields.file.details.image.width}
+                  alt={`Image of the editor of this article, ${release.editor.fullName}`}
+                  className="h-[100px] w-[100px] rounded-full"
+                />
+                <h2 className=" text-base font-bold text-neon-orange-500">
+                  {release.editor.fullName}
+                </h2>
+                <p className="text-center text-sm">{release.editor.role}</p>
+                <a
+                  href="mailto:psaunders@ifgsd.com"
+                  className="text-xs font-bold underline"
+                >
+                  Press Contact
+                </a>
+              </div>
+              <article className="mx-auto my-10 max-w-prose rounded-lg bg-white px-8 py-10">
+                {documentToReactComponents(release.writtenContent, options)}
+              </article>
+            </div>
           </Container>
         </section>
       </PublicLayout>
