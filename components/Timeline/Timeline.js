3701920e32b6baf4de407890b49f8f35cb8e46fd
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2023-12-08 14:59:36 -0800

    updates to timeline component styling

--- components/Timeline/Timeline.js
+++ components/Timeline/Timeline.js
@@ -3,38 +3,42 @@ import { Chrono } from "react-chrono";
 import Image from "next/image";
 
 const Timeline = ({ data }) => {
+  const formattedData = data.map((year) => {
+    return {
+      title: year.title,
+      cardTitle: year.cardTitle,
+      cardSubtitle: year.title,
+      cardDetailedText: year.cardDetailedText,
+      media: {
+        name: year.media ? year.media.fields.file.fileName : null,
+        source: {
+          url: year.media ? `https:${year.media.fields.file.url}` : null,
+        },
+        type: "IMAGE",
+      },
+    };
+  });
+
   return (
     <Chrono
-      items={data}
+      items={formattedData}
       mode="HORIZONTAL"
       showAllCardsHorizontal
+      mediaHeight={200}
       theme={{
         primary: "#F47E50",
         secondary: "#6AA9C0",
         titleColorActive: "white",
         titleColor: "white",
-        iconBackgroundColor: "white",
-        cardTitleColor: "white",
       }}
-    >
-      {data.map((item) => {
-        return (
-          <div className="min-h-[350px] w-[380px] overflow-y-hidden bg-white px-4">
-            {/* <Image
-              src={"https://placehold.co/400x100.png"}
-              width={400}
-              height={100}
-              className="mb-10"
-            /> */}
-            <h3 className="text-2xl font-bold text-neon-orange-500">
-              {item.cardTitle}
-            </h3>
-            <p className="mb-4 text-hazard-blue-500">{item.title}</p>
-            <p className="text-base">{item.cardDetailedText}</p>
-          </div>
-        );
-      })}
-    </Chrono>
+      classNames={{
+        card: "my-card",
+        cardTitle: "my-card-title",
+        cardSubTitle: "my-card-subtitle",
+        cardText: "my-card-text",
+        cardMedia: "my-card-media",
+      }}
+    />
   );
 };
 
