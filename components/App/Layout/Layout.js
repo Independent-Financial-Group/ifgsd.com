9cd66066d378e6651c677717ab893239627fd5b6
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2023-12-14 13:01:43 -0800

    setup desktop navbar

--- components/App/Layout/Layout.js
+++ components/App/Layout/Layout.js
@@ -2,14 +2,13 @@ import { Fragment, useState } from "react";
 import { Dialog, Transition, Disclosure } from "@headlessui/react";
 import {
   Bars3Icon,
-  CalendarIcon,
-  ChartPieIcon,
-  DocumentDuplicateIcon,
   FolderIcon,
   HomeIcon,
   UsersIcon,
   XMarkIcon,
   ChevronRightIcon,
+  PhoneIcon,
+  BuildingOffice2Icon,
 } from "@heroicons/react/24/outline";
 
 import { UserButton } from "@clerk/nextjs";
@@ -20,44 +19,55 @@ import Link from "next/link";
 import logoIcon from "../../../public/ifg-icon.png";
 
 const navigation = [
-  { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
+  { name: "Dashboard", href: "/app", icon: HomeIcon, current: true },
   {
     name: "Departments",
-    icon: UsersIcon,
+    icon: BuildingOffice2Icon,
     current: false,
     children: [
-      { name: "Advisory", links: [{ name: "overview", href: "#" }] },
-      { name: "Alternative Investments", href: "#" },
-      { name: "Annuities", href: "#" },
-      { name: "compliance", href: "#" },
-      { name: "Insurance", href: "#" },
-      { name: "Annuities", href: "#" },
-      { name: "Operations", href: "#" },
-      { name: "Practice Development", href: "#" },
-      { name: "Retirement Services", href: "#" },
-      { name: "Supervision", href: "#" },
+      {
+        name: "Advisory",
+        links: [
+          { name: "Overview", href: "#" },
+          { name: "Approved Products", href: "#" },
+        ],
+      },
+      {
+        name: "Alt. Investments",
+        links: [
+          { name: "Overview", href: "#" },
+          { name: "Approved Products", href: "#" },
+        ],
+      },
+      {
+        name: "Annuities",
+        links: [
+          { name: "Overview", href: "#" },
+          { name: "Approved Products", href: "#" },
+        ],
+      },
+      { name: "Compliance", links: [{ name: "Overview", href: "#" }] },
+      { name: "Insurance", links: [{ name: "Overview", href: "#" }] },
+      { name: "Annuities", links: [{ name: "Overview", href: "#" }] },
+      { name: "Operations", links: [{ name: "Overview", href: "#" }] },
+      {
+        name: "Practice Development",
+        links: [{ name: "overview", href: "#" }],
+      },
+      { name: "Retirement Services", links: [{ name: "overview", href: "#" }] },
+      { name: "Supervision", links: [{ name: "overview", href: "#" }] },
     ],
   },
   {
-    name: "Projects",
+    name: "Resources",
     icon: FolderIcon,
     current: false,
     children: [
-      { name: "GraphQL API", href: "#" },
-      { name: "iOS App", href: "#" },
-      { name: "Android App", href: "#" },
-      { name: "New Customer Portal", href: "#" },
+      { name: "Podcast", href: "#" },
+      { name: "Directory", links: [{ name: "Home Office", href: "#" }] },
     ],
   },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
-];
-
-const teams = [
-  { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
-  { id: 2, name: "Tailwind Labs", href: "#", initial: "T", current: false },
-  { id: 3, name: "Workcation", href: "#", initial: "W", current: false },
+  { name: "Contact", current: false, icon: PhoneIcon },
 ];
 
 function classNames(...classes) {
@@ -126,7 +136,7 @@ export default function Layout({ children }) {
                   <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2">
                     <div className="flex h-16 shrink-0 items-center">
                       <Image
-                        className="h-8 w-auto"
+                        className="mx-auto h-8 w-auto"
                         src={logoIcon}
                         alt="ifg logo"
                       />
@@ -161,38 +171,6 @@ export default function Layout({ children }) {
                             ))}
                           </ul>
                         </li>
-                        <li>
-                          <div className="text-xs font-semibold leading-6 text-gray-400">
-                            Your teams
-                          </div>
-                          <ul role="list" className="-mx-2 mt-2 space-y-1">
-                            {teams.map((team) => (
-                              <li key={team.name}>
-                                <a
-                                  href={team.href}
-                                  className={classNames(
-                                    team.current
-                                      ? "bg-gray-50 text-indigo-600"
-                                      : "text-gray-700 hover:bg-gray-50 hover:text-indigo-600",
-                                    "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
-                                  )}
-                                >
-                                  <span
-                                    className={classNames(
-                                      team.current
-                                        ? "border-indigo-600 text-indigo-600"
-                                        : "border-gray-200 text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600",
-                                      "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium",
-                                    )}
-                                  >
-                                    {team.initial}
-                                  </span>
-                                  <span className="truncate">{team.name}</span>
-                                </a>
-                              </li>
-                            ))}
-                          </ul>
-                        </li>
                       </ul>
                     </nav>
                   </div>
@@ -205,97 +183,129 @@ export default function Layout({ children }) {
         {/* Static sidebar for desktop */}
         <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
           {/* Sidebar component, swap this element with another sidebar if you like */}
-          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-4">
             <div className="flex h-16 shrink-0 items-center">
-              <Image className="h-8 w-auto" src={logoIcon} alt="Your Company" />
+              <Image
+                className="mx-auto h-8 w-auto"
+                src={logoIcon}
+                alt="Your Company"
+              />
             </div>
-            <nav className="flex flex-1 flex-col">
-              <ul role="list" className="flex flex-1 flex-col gap-y-7">
-                <li>
-                  <ul role="list" className="-mx-2 space-y-1">
-                    {navigation.map((item) => (
-                      <li key={item.name}>
-                        {!item.children ? (
-                          <a
-                            href={item.href}
-                            className={classNames(
-                              item.current ? "bg-gray-50" : "hover:bg-gray-50",
-                              "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700",
-                            )}
-                          >
-                            <item.icon
-                              className="h-6 w-6 shrink-0 text-gray-400"
-                              aria-hidden="true"
-                            />
-                            {item.name}
-                          </a>
-                        ) : (
-                          <Disclosure as="div">
-                            {({ open }) => (
+            <nav>
+              <ul className="flex flex-col gap-y-5">
+                {/* LOOP THROUGH NAVIGATION ARRAY */}
+                {navigation.map((parentItem) => {
+                  return (
+                    <li>
+                      {/* RENDER A DISCLOSURE FOR THE PARENT ITEM IN THE NAVIGATION ARRAY */}
+                      <Disclosure as="div">
+                        {({ open }) => (
+                          <>
+                            {/* DOES THE PARENT ITEM HAVE CHILDREN?*/}
+                            {parentItem.children ? (
                               <>
-                                <Disclosure.Button
-                                  className={classNames(
-                                    item.current
-                                      ? "bg-gray-50"
-                                      : "hover:bg-gray-50",
-                                    "flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm font-semibold leading-6 text-gray-700",
-                                  )}
-                                >
-                                  <item.icon
-                                    className="h-6 w-6 shrink-0 text-gray-400"
+                                <Disclosure.Button className="group flex w-full items-center gap-x-3 rounded-lg px-2 py-2 font-bold text-neon-orange-600 hover:bg-neon-orange-100 hover:text-neon-orange-500">
+                                  <parentItem.icon
+                                    className="h-6 w-6 shrink-0"
                                     aria-hidden="true"
                                   />
-                                  {item.name}
+                                  {parentItem.name}
                                   <ChevronRightIcon
-                                    className={classNames(
+                                    className={
                                       open
-                                        ? "rotate-90 text-gray-500"
-                                        : "text-gray-400",
-                                      "ml-auto h-5 w-5 shrink-0",
-                                    )}
-                                    aria-hidden="true"
+                                        ? "ml-auto h-5 w-auto rotate-90 transform"
+                                        : "ml-auto h-5 w-auto"
+                                    }
                                   />
                                 </Disclosure.Button>
-                                <Disclosure as="ul" className="mt-1 px-2">
-                                  {item.link.map((subItem) => (
-                                    <li key={subItem.name}>
-                                      {/* 44px */}
-                                      <Disclosure.Button
-                                        as="a"
-                                        href={subItem.href}
-                                        className={classNames(
-                                          subItem.current
-                                            ? "bg-gray-50"
-                                            : "hover:bg-gray-50",
-                                          "block rounded-md py-2 pl-9 pr-2 text-sm leading-6 text-gray-700",
-                                        )}
-                                      >
-                                        {subItem.name}
-                                      </Disclosure.Button>
-                                      <Disclosure.Panel>sub</Disclosure.Panel>
-                                    </li>
-                                  ))}
-                                </Disclosure>
+                                <Disclosure.Panel className="ml-4 mt-2">
+                                  <ul className="flex flex-col">
+                                    {/* LOOP OVER THE CHILD ITEMS */}
+                                    {parentItem.children.map((childItem) => {
+                                      return (
+                                        <>
+                                          {/* DOES THE CHILD ITEM HAVE LINKS? */}
+                                          {childItem.links ? (
+                                            <Disclosure>
+                                              {({ open }) => {
+                                                return (
+                                                  <>
+                                                    <Disclosure.Button className="flex w-full items-center justify-between rounded-lg p-2 font-bold text-hazard-blue-700 hover:bg-hazard-blue-100 hover:text-hazard-blue-500">
+                                                      {childItem.name}
+                                                      <ChevronRightIcon
+                                                        className={
+                                                          open
+                                                            ? "h-5 w-auto rotate-90 transform"
+                                                            : "h-5 w-auto"
+                                                        }
+                                                      />
+                                                    </Disclosure.Button>
+                                                    <Disclosure.Panel>
+                                                      <ul>
+                                                        {/* LOOP OVER THE LINKS */}
+                                                        {childItem.links.map(
+                                                          (link) => {
+                                                            return (
+                                                              <Disclosure
+                                                                as="ul"
+                                                                className="ml-4"
+                                                              >
+                                                                <Disclosure.Button
+                                                                  as="li"
+                                                                  className="rounded-lg p-2 hover:bg-gray-100 hover:text-gray-500"
+                                                                >
+                                                                  <Link
+                                                                    href={
+                                                                      link.href
+                                                                    }
+                                                                    className="block w-full"
+                                                                  >
+                                                                    {link.name}
+                                                                  </Link>
+                                                                </Disclosure.Button>
+                                                              </Disclosure>
+                                                            );
+                                                          },
+                                                        )}
+                                                      </ul>
+                                                    </Disclosure.Panel>
+                                                  </>
+                                                );
+                                              }}
+                                            </Disclosure>
+                                          ) : (
+                                            <Link
+                                              className="flex w-full items-center justify-between rounded-lg p-2 font-bold text-hazard-blue-700 hover:bg-hazard-blue-100 hover:text-hazard-blue-500"
+                                              href={childItem.href}
+                                            >
+                                              {childItem.name}
+                                            </Link>
+                                          )}
+                                        </>
+                                      );
+                                    })}
+                                  </ul>
+                                </Disclosure.Panel>
                               </>
+                            ) : (
+                              <Disclosure>
+                                <Disclosure.Button className="flex w-full items-center gap-x-3 rounded-lg p-2 font-bold text-neon-orange-600 hover:bg-neon-orange-100 hover:text-neon-orange-500">
+                                  <parentItem.icon
+                                    className="h-6 w-6 shrink-0"
+                                    aria-hidden="true"
+                                  />
+                                  <Link href="parentItem.href">
+                                    {parentItem.name}
+                                  </Link>
+                                </Disclosure.Button>
+                              </Disclosure>
                             )}
-                          </Disclosure>
+                          </>
                         )}
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-                <li className="-mx-6 mt-auto">
-                  <div
-                    href="#"
-                    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
-                  >
-                    <UserButton
-                      userProfileMode="navigation"
-                      userProfileUrl="/app/account"
-                      afterSignOutUrl="/"
-                    />
-                  </div>
-                </li>
+                      </Disclosure>
+                    </li>
+                  );
+                })}
               </ul>
             </nav>
           </div>
