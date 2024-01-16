72ae12f5d183d44f07c2e2db096e35a9f40b6c1a
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-01-16 08:07:36 -0800

    update to only show password screen for app route

--- middleware.js
+++ middleware.js
@@ -4,20 +4,21 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: [
-    "/",
-    "/reg-bi-investor",
-    "/privacy-policy",
-    "/disclosures",
-    "/join/(.*)",
-    "/about/(.*)",
-    "/careers(.*)",
-    "/press-and-media/(.*)",
-    "/offerings(.*)",
-    "/find-an-advisor",
-    "/api/public(.*)",
-    "/advisor-lookup",
-  ],
+  // publicRoutes: [
+  //   "/",
+  //   "/reg-bi-investor",
+  //   "/privacy-policy",
+  //   "/disclosures",
+  //   "/join/(.*)",
+  //   "/about/(.*)",
+  //   "/careers(.*)",
+  //   "/press-and-media/(.*)",
+  //   "/offerings(.*)",
+  //   "/find-an-advisor",
+  //   "/api/public(.*)",
+  //   "/advisor-lookup",
+  // ],
+  publicRoutes: ["((?!^/app/).*)"],
 });
 
 export const config = {
