9626ced9d573a2c82987b73d5695ef39288c7841
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-01-22 11:24:00 -0800

    new page

---
+++ pages/app/alternative-investments/overview.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Layout from "../../../components/App/Layout/Layout";
+import PageHeader from "../../../components/App/Internal Pages/PageHeader/PageHeader";
+import Head from "next/head";
+import OverviewVideo from "../../../components/App/Internal Pages/overview/overviewVideo/OverviewVideo";
+import DepartmentAnnouncements from "../../../components/App/Internal Pages/overview/DepartmentAnnouncements/DepartmentAnnouncements";
+import ContentLibrary from "../../../components/App/Internal Pages/overview/ContentLibrary/ContentLibrary";
+
+const overview = () => {
+  return (
+    <>
+      <Head>
+        <title>Overview | Alternative Investments</title>
+        <meta
+          name="description"
+          content="An overview of the Alternative Investments Department at IFG."
+        />
+      </Head>
+      <Layout>
+        <PageHeader
+          pageName={"Alternative Investments Overview"}
+          headerText={
+            "Within the Alternative Investments web pages, you have access to DPP, REIT and 1031 offerings as well as latest news."
+          }
+        />
+        <div className="my-10 flex flex-col gap-5 px-8 xl:grid xl:grid-cols-12 xl:gap-5">
+          <OverviewVideo />
+          <DepartmentAnnouncements department={"alts"} />
+          <ContentLibrary />
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default overview;
