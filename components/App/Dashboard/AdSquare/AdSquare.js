d48475630dfc12d11af26a56e2a570527a08e309
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-01-23 08:36:55 -0800

    new component

---
+++ components/App/Dashboard/AdSquare/AdSquare.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AdSquare = () => {
+  return (
+    <div className="col-span-4">
+      <a href="https://nationwide.com" target="_blank" rel="noopener">
+        <img
+          src="https://mdfarmbureau.com/wp-content/uploads/2018/11/AFO-1105AO_893x893_Farm-Bureau-Co-Branding.jpg"
+          alt="nationwide ad"
+          className="w-full rounded-lg object-cover shadow-lg"
+        />
+      </a>
+    </div>
+  );
+};
+
+export default AdSquare;
