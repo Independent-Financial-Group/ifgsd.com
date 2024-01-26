524012a1557ac3ddf3bfd619f18049f1bfa43bb0
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-01-26 11:11:29 -0800

    add breadcumb

--- components/App/Internal-Pages/PageHeader/PageHeader.js
+++ components/App/Internal-Pages/PageHeader/PageHeader.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-const PageHeader = ({ pageName, headerText }) => {
+const PageHeader = ({ pageName, headerText, breadCrumb }) => {
   return (
     <header className="relative h-[20dvh] bg-neon-orange-500">
       <div className="h-fullpx-8 py-4">
         <div className="absolute inset-x-8 top-1/2 -translate-y-1/2">
+          {breadCrumb && (
+            <p className="text-sm font-medium text-seabreeze-500">
+              {breadCrumb}
+            </p>
+          )}
           <h1 className="text-3xl font-bold text-seabreeze-500">{pageName}</h1>
-          <p className=" text-pretty leading-7 text-seabreeze-500">
-            {headerText}
-          </p>
+          {headerText && (
+            <p className=" text-pretty leading-7 text-seabreeze-500">
+              {headerText}
+            </p>
+          )}
         </div>
       </div>
     </header>
