c081f55d53f5f0fe783e02a65278f4af01e4f7b2
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-02-01 16:48:32 -0800

    Conditional path building based on content type and topic

--- pages/api/public/preview.js
+++ pages/api/public/preview.js
@@ -1,9 +1,28 @@
 import { COOKIE_NAME_PRERENDER_BYPASS } from "next/dist/server/api-utils";
 
 export default function handler(req, res) {
-  const { slug } = req.query;
+  const { slug, contentType, topic } = req.query;
   res.setPreviewData({});
   res.setDraftMode({ enable: true });
+
+  let pathPrefix;
+  const createPath = () => {
+    switch (contentType) {
+      case "pressRelease":
+        pathPrefix = "/press-and-media/press-releases/";
+        break;
+      case "marketCommentary":
+        if (topic == "Metrics that Matter") {
+          pathPrefix = "/app/portfolio-construction/market-commentary/MTM/";
+        } else if (topic == "Portfolio Construction Research") {
+          pathPrefix =
+            "/app/portfolio-construction/market-commentary/portfolio-construction-research/";
+        }
+        break;
+    }
+  };
+
+  createPath();
 
   const headers = res.getHeader("Set-Cookie");
   if (Array.isArray(headers)) {
@@ -17,6 +36,9 @@ export default function handler(req, res) {
       }),
     );
   }
+  console.log(`PREFIX: ${pathPrefix}`);
+  console.log(`SLUG: ${slug}`);
+  console.log(`FULL PATH: ${pathPrefix.concat(slug)}`);
 
-  res.redirect(slug);
+  res.redirect(pathPrefix.concat(slug));
 }
