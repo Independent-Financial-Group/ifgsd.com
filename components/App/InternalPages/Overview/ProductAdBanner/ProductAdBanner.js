da00665e9c51abe51302000e22ccf23ca40ce748
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-02-02 09:53:25 -0800

    use prop for URL

--- components/App/InternalPages/Overview/ProductAdBanner/ProductAdBanner.js
+++ components/App/InternalPages/Overview/ProductAdBanner/ProductAdBanner.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const ProductAdBanner = () => {
+const ProductAdBanner = ({ url }) => {
   return (
     <a
       href="https://www.sealynet.com/"
@@ -8,11 +8,7 @@ const ProductAdBanner = () => {
       target="_blank"
       className="col-span-12 block"
     >
-      <img
-        src="https://images.ctfassets.net/9lvru9ro1ti1/2mtjSuITOLE31fF5OV45XY/354efb69633de440ea2533a25c7f4220/sealy_banner_ad_2023.jpg"
-        alt="product ad banner"
-        className="rounded-lg shadow-lg"
-      />
+      <img src={url} alt="product ad banner" className="rounded-lg shadow-lg" />
     </a>
   );
 };
