139175dba3e45d84f1d585aa11ef82ea9344d7ce
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-02-02 09:52:45 -0800

    populate data from contentful

--- components/App/InternalPages/Overview/PortfolioConstruction/MarketResearch/MarketResearch.js
+++ components/App/InternalPages/Overview/PortfolioConstruction/MarketResearch/MarketResearch.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 import {
   MagnifyingGlassIcon,
   ChevronRightIcon,
 } from "@heroicons/react/24/outline";
+
+import * as contentful from "../../../../../../utils/contentful";
+import { formatDateAndTime } from "@contentful/f36-datetime";
 
 const MarketResearch = () => {
   const announcements = [
@@ -59,6 +62,23 @@ const MarketResearch = () => {
     },
   ];
 
+  const [marketCommentary, setMarketCommentary] = useState([]);
+
+  const getMarketCommentary = async () => {
+    const data = await contentful.client
+      .getEntries({
+        content_type: "marketCommentary",
+        order: "-fields.date",
+      })
+      .then((response) => setMarketCommentary([...response.items]));
+
+    console.log(marketCommentary);
+  };
+
+  useEffect(() => {
+    getMarketCommentary();
+  }, []);
+
   return (
     <div className="col-span-4 h-[500px] rounded-lg bg-white shadow">
       <div className="rounded-t-lg bg-hazard-blue-500 py-2">
@@ -69,20 +89,25 @@ const MarketResearch = () => {
       </div>
       <div className="h-[90%] overflow-y-auto pl-4 pr-1 pt-4">
         <ol className="divide-y divide-gray-100">
-          {announcements.map((announcement) => (
-            <li key={announcement.title} className="flex gap-x-4 px-3 py-5">
+          {marketCommentary.map((article) => (
+            <li key={article.sys.id} className="flex gap-x-4 px-3 py-5">
               <div className="min-w-0">
                 <p className="text-sm font-semibold leading-6 text-gray-900">
-                  {announcement.title}
+                  {article.fields.title}
                 </p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                  {announcement.date}
-                </p>
+                <div className="flex gap-2">
+                  <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                    {formatDateAndTime(article.fields.date, "day")}
+                  </p>
+                  <button className="inline-flex items-center rounded-full bg-hazard-blue-100 px-2 py-1 text-xs font-medium text-hazard-blue-500 ring-1 ring-inset ring-hazard-blue-500/10">
+                    {article.fields.topic}
+                  </button>
+                </div>
                 <p className="my-2 text-xs leading-7 text-gray-900">
-                  {announcement.excerpt}
+                  {article.fields.excerpt}
                 </p>
                 <Link
-                  href="#"
+                  href={``}
                   className="flex items-center justify-end gap-1 text-xs font-semibold hover:text-neon-orange-600"
                 >
                   Read More <ChevronRightIcon className="h-3" />
