4cc6b4c84d5f241efbfc3116b174bb64318b76af
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-02-09 15:00:02 -0800

    allow custom color through props

--- components/App/GridTile/GridTile.js
+++ components/App/GridTile/GridTile.js
@@ -2,10 +2,16 @@ import React from "react";
 // THIS COMPONENT IS USED FOR DISPLAYING A GENERAL GRID TILE ON ANY PAGE. YOU CAN SET THE GRID COL SPAN
 // THE TILE TITLE AND THE CHILDREN CONTENT VIA PROPS
 
-const GridTile = ({ tileTitle, colSpan, children, scroll }) => {
+const GridTile = ({
+  tileTitle,
+  colSpan,
+  children,
+  scroll,
+  titleHex = "bg-hazard-blue-500",
+}) => {
   return (
     <section className={`${colSpan} rounded-lg bg-white shadow`}>
-      <div className="mb-5 rounded-t-lg bg-hazard-blue-500 py-2">
+      <div className={`mb-5 rounded-t-lg ${titleHex} py-2`}>
         <h2 className="ml-4 flex gap-2 font-bold text-seabreeze-500">
           {tileTitle}
         </h2>
