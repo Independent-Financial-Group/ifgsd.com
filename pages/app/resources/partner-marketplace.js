6e5cfdc41e926fcccf11084015ae24b74f8430d0
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-02-09 15:00:24 -0800

    set grid tile color based on partner level

--- pages/app/resources/partner-marketplace.js
+++ pages/app/resources/partner-marketplace.js
@@ -17,20 +17,83 @@ import * as contentful from "../../../utils/contentful";
 
 const partnerMarketplace = () => {
   const [elitePartners, setElitePartners] = useState([]);
+  const [platinumPartners, setPlatinumPartners] = useState([]);
+  const [goldPartners, setGoldPartners] = useState([]);
+  const [silverPartners, setSilverPartners] = useState([]);
 
   const getPartners = async () => {
-    const data = await contentful.client
+    const eliteData = await contentful.client
       .getEntries({
         content_type: "partners",
+        "fields.partnerLevel[match]": "Elite",
+        order: "fields.partnerName",
       })
       .then((response) => {
         setElitePartners([...response.items]);
+      });
+    const platinumData = await contentful.client
+      .getEntries({
+        content_type: "partners",
+        "fields.partnerLevel[match]": "Platinum",
+        order: "fields.partnerName",
+      })
+      .then((response) => {
+        setPlatinumPartners([...response.items]);
+      });
+    const goldData = await contentful.client
+      .getEntries({
+        content_type: "partners",
+        "fields.partnerLevel[match]": "Gold",
+        order: "fields.partnerName",
+      })
+      .then((response) => {
+        setGoldPartners([...response.items]);
+      });
+    const silverData = await contentful.client
+      .getEntries({
+        content_type: "partners",
+        "fields.partnerLevel[match]": "Silver",
+        order: "fields.partnerName",
+      })
+      .then((response) => {
+        setSilverPartners([...response.items]);
       });
   };
 
   useEffect(() => {
     getPartners();
   }, []);
+
+  const splideOptions = {
+    type: "slide",
+    pagination: true,
+    arrows: true,
+    gap: "80px",
+    classes: {
+      arrow:
+        "bg-neon-orange-500 absolute rounded-full p-2 z-50 disabled:bg-gray-100 h-10 w-10 [&>svg]:disabled:!fill-gray-500 [&>svg]:!fill-seabreeze-500 [&>svg]:!w-6 [&>svg]:!h-6 top-1/2 -translate-y-1/2",
+      pagination: "flex items-center justify-center gap-3",
+      page: "bg-hazard-blue-500 z-50 h-2 w-2 rounded-full",
+    },
+    breakpoints: {
+      640: {
+        perPage: 1,
+      },
+      768: {
+        perPage: 2,
+      },
+      1024: {
+        perPage: 4,
+      },
+      1280: {
+        perPage: 4,
+      },
+      1536: {
+        perPage: 4,
+      },
+    },
+    padding: { left: 80, right: 80 },
+  };
 
   return (
     <>
@@ -40,145 +103,118 @@ const partnerMarketplace = () => {
       <Layout>
         <PageHeader pageName="Partner Marketplace" breadCrumb="Resources" />
         <ContentContainer>
-          <GridTile colSpan={"col-span-6 pb-5"} tileTitle={"Elite Partners"}>
+          <GridTile
+            colSpan={"col-span-full pb-5"}
+            tileTitle={"Elite Partners"}
+            titleHex="bg-gradient-to-r from-red-500 to-orange-500"
+          >
             <Splide
               aria-label="My Favorite Images"
-              options={{
-                type: "loop",
-                perPage: 2,
-                pagination: false,
-                arrows: false,
-                gap: "50px",
-                autoScroll: {
-                  speed: 0.3,
-                },
-                autoplay: true,
-              }}
+              options={splideOptions}
               tag="div"
-              extensions={{ AutoScroll }}
             >
               {elitePartners.map((partner) => (
                 <SplideSlide key={partner.fields.partnerName}>
                   <div className="flex flex-col items-center justify-center">
-                    <img
-                      src={`https:${partner.fields.logo.fields.file.url}`}
-                      alt={partner.fields.partnerName}
-                      className="width-[25%] aspect-[3/2] object-contain"
-                    />
-                    <Link
-                      href={"#"}
-                      className="mt-2 w-fit rounded-lg bg-neon-orange-500 p-2 text-white"
+                    <a
+                      href={partner.fields.linkToPartnerWebsite}
+                      target="_blank"
+                      rel="nofollow noopener"
+                      className="hover:cursor-pointer"
                     >
-                      Learn More
-                    </Link>
+                      <img
+                        src={`https:${partner.fields.logo.fields.file.url}`}
+                        alt={partner.fields.partnerName}
+                        className="width-[25%] aspect-[3/2] object-contain"
+                      />
+                    </a>
                   </div>
                 </SplideSlide>
               ))}
             </Splide>
           </GridTile>
-          <GridTile colSpan={"col-span-6 pb-5"} tileTitle={"Platinum Partners"}>
+          <GridTile
+            colSpan={"col-span-full pb-5"}
+            tileTitle={"Platinum Partners"}
+            titleHex="bg-gradient-to-r from-green-400 via-green-200 to-green-400"
+          >
             <Splide
               aria-label="My Favorite Images"
-              options={{
-                type: "loop",
-                perPage: 2,
-                pagination: false,
-                arrows: false,
-                gap: "50px",
-                autoScroll: {
-                  speed: 0.3,
-                },
-                autoplay: true,
-              }}
+              options={splideOptions}
               tag="div"
-              extensions={{ AutoScroll }}
             >
-              {elitePartners.map((partner) => (
+              {platinumPartners.map((partner) => (
                 <SplideSlide key={partner.fields.partnerName}>
                   <div className="flex flex-col items-center justify-center">
-                    <img
-                      src={`https:${partner.fields.logo.fields.file.url}`}
-                      alt={partner.fields.partnerName}
-                      className="width-[25%] aspect-[3/2] object-contain"
-                    />
-                    <Link
-                      href={"#"}
-                      className="mt-2 w-fit rounded-lg bg-neon-orange-500 p-2 text-white"
+                    <a
+                      href={partner.fields.linkToPartnerWebsite}
+                      target="_blank"
+                      rel="nofollow noopener"
                     >
-                      Learn More
-                    </Link>
+                      <img
+                        src={`https:${partner.fields.logo.fields.file.url}`}
+                        alt={partner.fields.partnerName}
+                        className="width-[25%] aspect-[3/2] object-contain"
+                      />
+                    </a>
                   </div>
                 </SplideSlide>
               ))}
             </Splide>
           </GridTile>
-          <GridTile colSpan={"col-span-6 pb-5"} tileTitle={"Gold Partners"}>
+          <GridTile
+            colSpan={"col-span-full pb-5"}
+            tileTitle={"Gold Partners"}
+            titleHex="bg-gradient-to-r from-yellow-400 to-yellow-300"
+          >
             <Splide
               aria-label="My Favorite Images"
-              options={{
-                type: "loop",
-                perPage: 2,
-                pagination: false,
-                arrows: false,
-                gap: "50px",
-                autoScroll: {
-                  speed: 0.3,
-                },
-                autoplay: true,
-              }}
+              options={splideOptions}
               tag="div"
-              extensions={{ AutoScroll }}
             >
-              {elitePartners.map((partner) => (
+              {goldPartners.map((partner) => (
                 <SplideSlide key={partner.fields.partnerName}>
                   <div className="flex flex-col items-center justify-center">
-                    <img
-                      src={`https:${partner.fields.logo.fields.file.url}`}
-                      alt={partner.fields.partnerName}
-                      className="width-[25%] aspect-[3/2] object-contain"
-                    />
-                    <Link
-                      href={"#"}
-                      className="mt-2 w-fit rounded-lg bg-neon-orange-500 p-2 text-white"
+                    <a
+                      href={partner.fields.linkToPartnerWebsite}
+                      target="_blank"
+                      rel="nofollow noopener"
                     >
-                      Learn More
-                    </Link>
+                      <img
+                        src={`https:${partner.fields.logo.fields.file.url}`}
+                        alt={partner.fields.partnerName}
+                        className="width-[25%] aspect-[3/2] object-contain"
+                      />
+                    </a>
                   </div>
                 </SplideSlide>
               ))}
             </Splide>
           </GridTile>
-          <GridTile colSpan={"col-span-6 pb-5"} tileTitle={"Silver Partners"}>
+          <GridTile
+            colSpan={"col-span-full pb-5"}
+            tileTitle={"Silver Partners"}
+            titleHex="bg-gradient-to-r from-gray-300 to-gray-500"
+          >
             <Splide
               aria-label="My Favorite Images"
-              options={{
-                type: "loop",
-                perPage: 2,
-                pagination: false,
-                arrows: false,
-                gap: "50px",
-                autoScroll: {
-                  speed: 0.3,
-                },
-                autoplay: true,
-              }}
+              options={splideOptions}
               tag="div"
-              extensions={{ AutoScroll }}
             >
-              {elitePartners.map((partner) => (
+              {silverPartners.map((partner) => (
                 <SplideSlide key={partner.fields.partnerName}>
                   <div className="flex flex-col items-center justify-center">
-                    <img
-                      src={`https:${partner.fields.logo.fields.file.url}`}
-                      alt={partner.fields.partnerName}
-                      className="width-[25%] aspect-[3/2] object-contain"
-                    />
-                    <Link
-                      href={"#"}
-                      className="mt-2 w-fit rounded-lg bg-neon-orange-500 p-2 text-white"
+                    <a
+                      href={partner.fields.linkToPartnerWebsite}
+                      target="_blank"
+                      rel="nofollow noopener"
                     >
-                      Learn More
-                    </Link>
+                      <img
+                        src={`https:${partner.fields.logo.fields.file.url}`}
+                        alt={partner.fields.partnerName}
+                        className="width-[25%] aspect-[3/2] object-contain"
+                      />
+                    </a>
                   </div>
                 </SplideSlide>
               ))}
