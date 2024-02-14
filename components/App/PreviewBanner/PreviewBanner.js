f9a9809d58d2c35ac9907eccd020af8298716216
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-02-14 15:04:37 -0800

    update language

--- components/App/PreviewBanner/PreviewBanner.js
+++ components/App/PreviewBanner/PreviewBanner.js
@@ -7,14 +7,14 @@ const PreviewBanner = () => {
   console.log(router.asPath);
 
   return (
-    <div className="sticky top-0 bg-neon-orange-600 px-4 py-3 text-white">
+    <div className="sticky top-0 z-50 bg-neon-orange-600 px-4 py-3 text-white">
       <p className="text-center text-sm font-medium">
-        You are viewing this page in draft mode.{" "}
+        You are viewing this page in preview mode.{" "}
         <a
           href={`/api/public/clear-preview?path=${router.asPath}`}
           className="inline-block underline"
         >
-          Click here to exit draft mode.
+          Click here to exit preview mode.
         </a>
       </p>
     </div>
