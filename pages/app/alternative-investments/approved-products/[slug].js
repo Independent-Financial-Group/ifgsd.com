2df9a083829d657ab7268dfbc6d86e33fb7e24cb
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-02-28 09:52:44 -0800

    condition to show details

--- pages/app/alternative-investments/approved-products/[slug].js
+++ pages/app/alternative-investments/approved-products/[slug].js
@@ -77,94 +77,100 @@ const ProductPage = ({ product, preview }) => {
         breadCrumb="Alternative Investments > Approved Product"
       />
       <ContentContainer>
-        <GridTile tileTitle="Product Overview" colSpan="col-span-6">
-          {product.fields.productType !== "1031s" && (
-            <ol className="grid grid-cols-2 gap-5 pb-5 text-sm [&_h3]:font-semibold [&_li]:gap-2">
-              <li>
-                <h3>Sponsor</h3>
-                <p>{product.fields.sponsorName}</p>
-              </li>
-              <li>
-                <h3>Offering</h3>
-                <p>{product.fields.offeringName}</p>
-              </li>
-              <li>
-                <h3>Type</h3>
-                <p>{product.fields.productTypeCategory}</p>
-              </li>
-              <li>
-                <h3>Objective</h3>
-                <p>{product.fields.objective}</p>
-              </li>
-              <li>
-                <h3>Suitability</h3>
-                <p>{product.fields.suitability}</p>
-              </li>
-              <li>
-                <h3>Minimum</h3>
-                <p>{product.fields.minimum}</p>
-              </li>
-              <li>
-                <h3>Status</h3>
-                <p>{product.fields.status}</p>
-              </li>
-              <li>
-                <h3>Approval Date</h3>
-                <p>{product.fields.approvalDate}</p>
-              </li>
-              <li>
-                <h3>Contact</h3>
-                <p>{product.fields.contact}</p>
-                <p>{product.fields.phone}</p>
-              </li>
-            </ol>
-          )}
-        </GridTile>
-        <GridTile
-          scroll
-          tileTitle="Supporting Documents"
-          colSpan={"col-span-6"}
-        >
-          <ul className="flex flex-col gap-3">
-            {!product.fields.supportingDocuments && (
-              <div className="flex items-center">
-                <p className="w-full text-center font-bold uppercase text-gray-500">
-                  No Documents
-                </p>
-              </div>
-            )}
-            {product.fields.supportingDocuments &&
-              product.fields.supportingDocuments.map((document) => (
-                <li
-                  key={document.sys.id}
-                  className="rounded bg-neon-orange-100 p-2"
-                >
-                  <Link
-                    href={`https:${document.fields.file.url}`}
-                    className="flex items-center gap-2"
-                    download
-                    target="_blank"
-                  >
-                    <div className="w-fit">
-                      <DocumentArrowDownIcon className="block h-10 w-10 text-neon-orange-500" />
-                    </div>
-                    <p className="line-clamp-1 text-sm font-bold text-neon-orange-500">
-                      {document.fields.title}
+        {preview && (
+          <>
+            <GridTile tileTitle="Product Overview" colSpan="col-span-6">
+              {product.fields.productType !== "1031s" && (
+                <ol className="grid grid-cols-2 gap-5 pb-5 text-sm [&_h3]:font-semibold [&_li]:gap-2">
+                  <li>
+                    <h3>Sponsor</h3>
+                    <p>{product.fields.sponsorName}</p>
+                  </li>
+                  <li>
+                    <h3>Offering</h3>
+                    <p>{product.fields.offeringName}</p>
+                  </li>
+                  <li>
+                    <h3>Type</h3>
+                    <p>{product.fields.productTypeCategory}</p>
+                  </li>
+                  <li>
+                    <h3>Objective</h3>
+                    <p>{product.fields.objective}</p>
+                  </li>
+                  <li>
+                    <h3>Suitability</h3>
+                    <p>{product.fields.suitability}</p>
+                  </li>
+                  <li>
+                    <h3>Minimum</h3>
+                    <p>{product.fields.minimum}</p>
+                  </li>
+                  <li>
+                    <h3>Status</h3>
+                    <p>{product.fields.status}</p>
+                  </li>
+                  <li>
+                    <h3>Approval Date</h3>
+                    <p>{product.fields.approvalDate}</p>
+                  </li>
+                  <li>
+                    <h3>Contact</h3>
+                    <p>{product.fields.contact}</p>
+                    <p>{product.fields.phone}</p>
+                  </li>
+                </ol>
+              )}
+            </GridTile>
+            <GridTile
+              scroll
+              tileTitle="Supporting Documents"
+              colSpan={"col-span-6"}
+            >
+              <ul className="flex flex-col gap-3">
+                {!product.fields.supportingDocuments && (
+                  <div className="flex items-center">
+                    <p className="w-full text-center font-bold uppercase text-gray-500">
+                      No Documents
                     </p>
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        </GridTile>
-        <div className="col-span-6">
-          <h2 className="text-lg font-bold text-neon-orange-500">Details</h2>
-          <Markdown
-            className="text-sm leading-7 [&_a]:font-semibold [&_a]:text-blue-wave-500 [&_p]:mb-2"
-            remarkPlugins={[remarkGfm]}
-          >
-            {product.fields.productWriteUp}
-          </Markdown>
-        </div>
+                  </div>
+                )}
+                {product.fields.supportingDocuments &&
+                  product.fields.supportingDocuments.map((document) => (
+                    <li
+                      key={document.sys.id}
+                      className="rounded bg-neon-orange-100 p-2"
+                    >
+                      <Link
+                        href={`https:${document.fields.file.url}`}
+                        className="flex items-center gap-2"
+                        download
+                        target="_blank"
+                      >
+                        <div className="w-fit">
+                          <DocumentArrowDownIcon className="block h-10 w-10 text-neon-orange-500" />
+                        </div>
+                        <p className="line-clamp-1 text-sm font-bold text-neon-orange-500">
+                          {document.fields.title}
+                        </p>
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+            </GridTile>
+            <div className="col-span-6">
+              <h2 className="text-lg font-bold text-neon-orange-500">
+                Details
+              </h2>
+              <Markdown
+                className="text-sm leading-7 [&_a]:font-semibold [&_a]:text-blue-wave-500 [&_p]:mb-2"
+                remarkPlugins={[remarkGfm]}
+              >
+                {product.fields.productWriteUp}
+              </Markdown>
+            </div>
+          </>
+        )}
       </ContentContainer>
     </Layout>
   );
