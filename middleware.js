ebe3b8c0019303ee935bceb6c1c1d4e93f016fcd
Alex Garcia <agarcia@ifg-nl457l7xqh.lan>
2024-03-07 10:29:32 -0800

    attempt 3 - fix redirect loop

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
@@ -20,6 +20,7 @@ export default authMiddleware({
   ignoredRoutes: (req) => !req.url.includes("/app"),
   afterAuth: (auth, req) =>
     auth.isPublicRoute ? NextReponse.next() : undefined,
+  debug: process.env.NEXT_PUBLIC_VERCEL_ENV == "development" ? true : false,
 });
 
 export const config = {
