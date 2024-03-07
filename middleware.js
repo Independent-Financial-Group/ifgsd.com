042ed7164a75da94477d2ecde9b20e250b652ea8
Alex Garcia <agarcia@ifg-nl457l7xqh.lan>
2024-03-07 10:39:39 -0800

    attempt 4 - fix infinite redirect and update clerk

--- middleware.js
+++ middleware.js
@@ -4,22 +4,11 @@ import { NextRequest, NextResponse } from "next/server";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-export function loggingMiddleware(NextRequest) {
-  console.log(
-    `Received ${NextRequest.method} request to ${
-      NextRequest.url
-    } at ${new Date()}`,
-  );
-
-  return { status: 200 };
-}
 
 export default authMiddleware({
   //MAKE ALL ROUTES BUT /APP PUBLIC
   publicRoutes: (req) => !req.url.includes("/app"),
   ignoredRoutes: (req) => !req.url.includes("/app"),
-  afterAuth: (auth, req) =>
-    auth.isPublicRoute ? NextReponse.next() : undefined,
   debug: process.env.NEXT_PUBLIC_VERCEL_ENV == "development" ? true : false,
 });
 
