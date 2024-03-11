9f27f2afc6badf8b7aa64fbd5a44cebb73e9e929
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-03-11 11:54:43 -0700

    make image object fit cover to maintain aspect ratio

--- components/App/Dashboard/AdSquare/AdSquare.js
+++ components/App/Dashboard/AdSquare/AdSquare.js
@@ -58,7 +58,7 @@ const AdSquare = () => {
                 <img
                   src={ad.url}
                   alt={ad.description}
-                  className="mx-auto h-[500px] w-full rounded-lg object-contain xl:object-fill"
+                  className="mx-auto h-[500px] w-full rounded-lg  object-cover"
                 />
               </a>
             </SwiperSlide>
