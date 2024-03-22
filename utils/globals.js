cc2da2dda8a8a7acda449c6301ee2a1d77371ccb
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-03-22 09:41:28 -0700

    new file to store data that is used throughout site

---
+++ utils/globals.js
@@ -0,0 +1,18 @@
+const departments = [
+  { id: "All", title: "All" },
+  { id: "Advisory", title: "Advisory" },
+  { id: "Alternative Investments", title: "Alternative Investments" },
+  { id: "Annuities", title: "Annuities" },
+  { id: "Commissions", title: "Commissions" },
+  { id: "Compliance", title: "Compliance" },
+  { id: "Corporate Communications", title: "Corporate Communications" },
+  { id: "Home Office", title: "Home Office" },
+  { id: "Information Technology", title: "Information Technology" },
+  { id: "Insurance", title: "Insurance" },
+  { id: "Operations", title: "Operations" },
+  { id: "Portfolio Construction", title: "Portfolio Construction" },
+  { id: "Practice Development", title: "Practice Development" },
+  { id: "Supervision", title: "Supervision" },
+];
+
+export { departments };
