d7600382ed206b751b0eb21fa18a02999896d91e
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-04-12 10:50:40 -0700

    update site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://ifgsd.com",
+  siteUrl: "https://www.ifgsd.com",
   generateRobotsTxt: true, // (optional)
   exclude: ["/app/*"],
   // ...other options
