3ff89acc5d006003aaf90f81c4e1d66bf91a97ff
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-04-18 15:31:00 -0700

    add additional classes prop to modify single component

--- components/App/ContentContainer/ContentContainer.js
+++ components/App/ContentContainer/ContentContainer.js
@@ -1,8 +1,10 @@
 import React from "react";
 
-const ContentContainer = ({ children }) => {
+const ContentContainer = ({ children, additionalClasses }) => {
   return (
-    <div className="relative my-10 flex flex-col gap-5 px-8 xl:grid xl:grid-cols-12 xl:gap-5">
+    <div
+      className={`relative my-10 flex flex-col gap-5 px-8 xl:grid xl:grid-cols-12 xl:gap-5 ${additionalClasses}`}
+    >
       {children}
     </div>
   );
