a5f70a1868f349bc7025c5df860e1a0dfd7fef71
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-04-19 15:01:38 -0700

    add auto rows

--- components/App/ContentContainer/ContentContainer.js
+++ components/App/ContentContainer/ContentContainer.js
@@ -3,7 +3,7 @@ import React from "react";
 const ContentContainer = ({ children, additionalClasses }) => {
   return (
     <div
-      className={`relative my-10 flex flex-col gap-5 px-8 xl:grid xl:grid-cols-12 xl:gap-5 ${additionalClasses}`}
+      className={`relative my-10 flex flex-col gap-5 px-8 xl:grid xl:grid-cols-12 xl:gap-5 ${additionalClasses} xl:grid-flow-row xl:auto-rows-max`}
     >
       {children}
     </div>
