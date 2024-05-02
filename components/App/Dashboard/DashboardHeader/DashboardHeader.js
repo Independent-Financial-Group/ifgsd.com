a294a8cd3c980bfa6f3fdffb463f83bda620a5a2
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-05-02 11:29:17 -0700

    fix width and positioning for layout changes

--- components/App/Dashboard/DashboardHeader/DashboardHeader.js
+++ components/App/Dashboard/DashboardHeader/DashboardHeader.js
@@ -5,11 +5,13 @@ import { useSession } from "next-auth/react";
 const DashboardHeader = () => {
   const { data: session } = useSession();
 
+  console.log(session);
+
   return (
-    <header className="relative min-h-[10dvh] bg-[url('https://images.pexels.com/photos/1705254/pexels-photo-1705254.jpeg')] bg-cover bg-no-repeat">
+    <header className="relative min-h-[10dvh] bg-[url('https://images.pexels.com/photos/1705254/pexels-photo-1705254.jpeg')] bg-cover bg-no-repeat lg:ml-72">
       <div className="flex min-h-[10dvh] flex-col justify-center bg-white/25 px-8 py-4 backdrop-blur-md">
         <h1 className="text-3xl font-bold text-seabreeze-500">
-          Hello, {session.user.name}!
+          Hello, {session ? session.user.name : ""}!
         </h1>
       </div>
     </header>
--- components/App/Dashboard/Disclosure/Disclosure.js
+++ components/App/Dashboard/Disclosure/Disclosure.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Disclosure = () => {
   return (
-    <div className="sticky bottom-0 z-50 w-full bg-neon-orange-500 px-2">
+    <div className="sticky bottom-0 z-50 bg-neon-orange-500 px-2 lg:ml-72">
       <p className=" text-pretty text-xs italic leading-7 text-seabreeze-500">
         This is a restricted area of our website dedicated solely to our valued
         independent representatives. Announcements sent to representatives of
