9484ee3342543523463861d59d51e015c9d6022b
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-05-20 14:59:25 -0700

    fix class names

--- pages/app/portfolio-construction/market-commentary/portfolio-construction-research/[slug].js
+++ pages/app/portfolio-construction/market-commentary/portfolio-construction-research/[slug].js
@@ -7,7 +7,6 @@ import Head from "next/head";
 // CONTENTFUL IMPORTS
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import * as contentful from "../../../../../utils/contentful";
-import { useContentfulLiveUpdates } from "@contentful/live-preview/react";
 
 export async function getStaticPaths() {
   const response = await contentful.client.getEntries({
@@ -58,6 +57,7 @@ export const getStaticProps = async ({ params, preview }) => {
 };
 
 const Article = ({ article, preview }) => {
+  console.log(article.fields.content);
   return (
     <Layout preview={preview}>
       <div className="flex min-h-screen items-center justify-center">
@@ -69,7 +69,7 @@ const Article = ({ article, preview }) => {
             src={`https:${article.fields.thumbnail.fields.file.url}`}
             className="mx-auto rounded-xl"
           />
-          <div className="my-5= mx-auto max-w-prose">
+          <div className="mx-auto my-5 max-w-prose">
             {documentToReactComponents(article.fields.content, options)}
           </div>
           <Link
