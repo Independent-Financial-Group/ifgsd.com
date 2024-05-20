4290522aff8c259db3ffac1787e99e72efa59765
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-05-20 15:10:12 -0700

    update isDirectLink Filter

--- pages/app/resources/content-library/[slug].js
+++ pages/app/resources/content-library/[slug].js
@@ -31,7 +31,7 @@ export async function getStaticProps({ params, preview }) {
   const data = await client.getEntries({
     content_type: "contentLibrary",
     "fields.slug[match]": slug,
-    "fields.containsDirectLink[nin]": 1,
+    "fields.isDirectLink[nin]": false,
   });
 
   return {
