9cd183f026f24d616e868f804c07d54d0973c723
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-06-11 08:51:40 -0700

    add width prop for further customization

--- components/App/Popover/Popover.js
+++ components/App/Popover/Popover.js
@@ -3,10 +3,12 @@ import React from "react";
 // requires parent element to have group tailwind class for transition to work.
 // Also require parent to have position relative.
 
-const Popover = ({ text, top, right, bottom, left }) => {
+const Popover = ({ text, top, right, bottom, left, width }) => {
   return (
     <div
-      className={`group-transition-opacity absolute z-50 whitespace-nowrap rounded bg-black/75 p-2 text-xs text-white opacity-0  group-hover:opacity-100 group-hover:delay-500 group-hover:duration-200 ${top} ${right} ${bottom} ${left} scale-0 group-hover:scale-100`}
+      className={`group-transition-opacity absolute z-50 rounded bg-black/75 p-2 text-xs text-white opacity-0  group-hover:opacity-100 group-hover:delay-500 group-hover:duration-200 ${top} ${right} ${bottom} ${left} ${
+        width ? width : "whitespace-nowrap"
+      } scale-0 group-hover:scale-100`}
     >
       {text}
     </div>
