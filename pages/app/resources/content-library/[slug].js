f7c3ef7fd01e0e600f83c64f0274bff04c864eef
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-06-25 13:57:43 -0700

    setup content library slug page for displaying ce session videos

--- pages/app/resources/content-library/[slug].js
+++ pages/app/resources/content-library/[slug].js
@@ -2,8 +2,18 @@ import React, { useState } from "react";
 
 import Head from "next/head";
 import Layout from "components/App/Layout/Layout";
+import PageHeader from "components/App/InternalPages/PageHeader/PageHeader";
+import ContentContainer from "components/App/ContentContainer/ContentContainer";
+import OverviewVideo from "components/App/InternalPages/Overview/OverviewVideo/OverviewVideo";
+
+import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
+import { VideoCameraIcon } from "@heroicons/react/24/outline";
 
 import * as contentful from "utils/contentful";
+
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export async function getStaticPaths() {
   const slugs = await contentful.client.getEntries({
@@ -50,7 +60,33 @@ const index = ({ content, preview }) => {
       <Head>
         <title>{!null && content.fields.title} | Content Library</title>
       </Head>
-      <Layout preview={preview}></Layout>
+      <PageHeader
+        pageName={content.fields.title}
+        breadCrumb="Content Library"
+      />
+      <Layout preview={preview}>
+        <ContentContainer>
+          {content.fields.category == "Video" ? (
+            <section className="col-span-full gap-5 xl:grid xl:grid-cols-2">
+              <article className="rounded bg-white p-2">
+                <Markdown
+                  className="text-sm leading-7 [&_a]:font-semibold [&_a]:text-blue-wave-500 [&_p]:mb-2"
+                  remarkPlugins={[remarkGfm]}
+                >
+                  {content.fields.text}
+                </Markdown>
+              </article>
+              <div className="col-span-1">
+                <ReactPlayer
+                  width={"100"}
+                  url={content.fields.linkToVideo}
+                  controls
+                />
+              </div>
+            </section>
+          ) : null}
+        </ContentContainer>
+      </Layout>
     </>
   );
 };
