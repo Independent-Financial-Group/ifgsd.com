046edb60f575ea2dbc342aedf0c1f369679f2169
Alex Garcia <agarcia@IFG-NL457L7XQH.local>
2024-07-17 07:37:34 -0700

    add bg image for print

--- components/PageHeader/PageHeader.js
+++ components/PageHeader/PageHeader.js
@@ -5,12 +5,12 @@ import Container from "../Container/Container";
 const PageHeader = ({ children, bgPath, centerElement }) => {
   return (
     <header
-      className={`relative h-[calc(75vh-60px)] rounded-bl-[20px] rounded-br-[20px] bg-cover bg-center bg-no-repeat md:h-[calc(35vh-60px)] lg:h-[calc(50vh-60px)] lg:rounded-bl-[40px] lg:rounded-br-[40px] xl:h-[calc(50vh-60px)] ${bgPath}`}
+      className={`relative h-[calc(75vh-60px)] rounded-bl-[20px] rounded-br-[20px] bg-cover bg-center bg-no-repeat md:h-[calc(35vh-60px)] lg:h-[calc(50vh-60px)] lg:rounded-bl-[40px] lg:rounded-br-[40px] xl:h-[calc(50vh-60px)] print:${bgPath} ${bgPath}`}
     >
       <div className="h-full rounded-bl-[20px] rounded-br-[20px] bg-dunkel-blue-900/25 lg:rounded-bl-[40px] lg:rounded-br-[40px]">
         <Container>
           {centerElement && (
-            <div className="flex h-full w-full items-center lg:w-auto lg:h-auto lg:absolute lg:left-1/2  lg:top-1/2 lg:-translate-x-1/2 lg:-translate-y-1/2">
+            <div className="flex h-full w-full items-center lg:absolute lg:left-1/2 lg:top-1/2 lg:h-auto  lg:w-auto lg:-translate-x-1/2 lg:-translate-y-1/2">
               {children}
             </div>
           )}
