403dc0402b4836d9ebc0e8f18797f1f64a77b06c
Alex Garcia <agarcia@IFG-NL457L7XQH.garcia.local>
2024-07-18 12:13:24 -0700

    functionality for internship program form

--- pages/api/public/sendgrid.js
+++ pages/api/public/sendgrid.js
@@ -3,6 +3,9 @@ import sendgrid from "@sendgrid/mail";
 import ContactFormSubmittedInternal from "../../../emails/contactFormSubmittedInternal";
 import ContactFormSubmittedExternal from "../../../emails/contactFormSubmittedExternal";
 import AdvisorDirectoryContactForm from "../../../emails/advisorDirectoryContactForm";
+import InternshipProgramEmailInternal from "../../../emails/internshipProgramEmailInternal";
+
+// const fs = require("fs");
 
 export default async function (req, res) {
   sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
@@ -25,6 +28,63 @@ export default async function (req, res) {
       await sendgrid.send(sendOptionsInternal);
       res.status(200).send(`Message Sent Successfully`);
     } catch (error) {
+      res.status(400).send(`ERROR: ${error}`);
+    }
+  } else if (req.body.formName == "internship program form") {
+    const { firstName, lastName, email } = req.body;
+
+    // const toBase64 = (file) =>
+    //   new Promise((resolve, reject) => {
+    //     const reader = new FileReader();
+    //     reader.readAsDataURL(file);
+    //     reader.onload = () => {
+    //       let encoded = reader.result.toString().replace(/^data:(.*,)?/, "");
+    //       if (encoded.length % 4 > 0) {
+    //         encoded += "=".repeat(4 - (encoded.length % 4));
+    //       }
+    //       resolve(encoded);
+    //     };
+    //     reader.onerror = (error) => reject(error);
+    //   });
+
+    // const convertedResumeFile = await toBase64(req.body.resumeFile);
+    // const convertedCoverLetterFile = await toBase64(req.body.coverLetterFile);
+
+    // RENDER EMAIL GOING TO APPLICANT
+    const externalEmailHtml = render();
+
+    // RENDER EMAIL GOING TO HR
+    const internalEmailHtml = render(
+      <InternshipProgramEmailInternal formData={req.body} />,
+    );
+
+    // EMAIL OPTIONS FOR EMAIL GOING TO HR
+    const sendOptionsInternal = {
+      from: "noreply@ifgsd.com",
+      to: "agarcia@ifgsd.com",
+      subject: `${firstName} ${lastName} has applied for an Internship`,
+      html: internalEmailHtml,
+      attachments: [
+        {
+          content: req.body.base64Resume,
+          filename: "Resume.pdf",
+          type: "application/pdf",
+          disposition: "attachment",
+        },
+        {
+          content: req.body.base64CoverLetter,
+          filename: "Cover Letter.pdf",
+          type: "application/pdf",
+          disposition: "attachment",
+        },
+      ],
+    };
+
+    try {
+      await sendgrid.send(sendOptionsInternal);
+      res.status(200).send(`Message Sent Successfully`);
+    } catch (error) {
+      console.log(error);
       res.status(400).send(`ERROR: ${error}`);
     }
   } else {
