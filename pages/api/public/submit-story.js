881811b5cdb6e6b8094b2ff0954458c8eed04510
Alex Garcia <agarcia@IFG-NL457L7XQH.garcia.local>
2024-07-21 10:13:29 -0700

    update to address for testing

--- pages/api/public/submit-story.js
+++ pages/api/public/submit-story.js
@@ -2,15 +2,16 @@ import { render } from "@react-email/render";
 import sendgrid from "@sendgrid/mail";
 
 // import OrderFormSubmission from "/emails/orderFormSubmission.jsx";
+import StorySubmissionInternal from "emails/storySubmissionEmail.jsx";
 
 export default async function (req, res) {
   sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
 
-  //   const internalEmail = render(<OrderFormSubmission cart={req.body} />);
+  const internalEmail = render(<StorySubmissionInternal formData={req.body} />);
 
   const sendOptionsInternal = {
     from: "noreply@ifgsd.com",
-    to: "CorpComm@ifgsd.com",
+    to: "agarcia@ifgsd.com",
     subject: `New Story Submission from ${req.body.firstName} ${req.body.lastName}`,
     html: internalEmail,
   };
