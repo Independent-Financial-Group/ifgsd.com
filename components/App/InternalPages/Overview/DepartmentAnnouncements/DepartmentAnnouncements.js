81dfce30ece55497316c4114cc86c30eb1ee219e
Alex Garcia <agarcia@IFG-NL457L7XQH.garcia.local>
2024-07-25 10:40:33 -0700

    update col span to full

--- components/App/InternalPages/Overview/DepartmentAnnouncements/DepartmentAnnouncements.js
+++ components/App/InternalPages/Overview/DepartmentAnnouncements/DepartmentAnnouncements.js
@@ -5,7 +5,7 @@ import { formatDateAndTime } from "@contentful/f36-datetime";
 
 const DepartmentAnnouncements = ({ data, name }) => {
   return (
-    <div className="col-span-6 h-[500px] rounded-lg bg-white shadow">
+    <div className="col-span-full h-[500px] rounded-lg bg-white shadow">
       <div className="rounded-t-lg bg-hazard-blue-500 py-2">
         <h2 className="ml-4 flex gap-2 font-bold text-seabreeze-500">
           <MegaphoneIcon className="h-5 w-5" />
